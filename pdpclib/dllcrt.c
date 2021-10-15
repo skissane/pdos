0653be2dd797fb8cc6686fc10cbb8dd941562c35
Paul Edwards <mutazilah@gmail.com>
2021-10-15 11:41:32 +1100

    ensure assert is included too

--- pdpclib/dllcrt.c
+++ pdpclib/dllcrt.c
@@ -18,6 +18,7 @@
 #include <locale.h>
 #include <setjmp.h>
 #include <math.h>
+#include <assert.h>
 
 #include <windows.h>
 
@@ -50,6 +51,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDll, DWORD fdwReason, LPVOID lpvReserved)
         time(NULL);
         localeconv();
         setjmp(jmpenv);
+        assert(0);
         sin(0.0);
     }
 #endif
