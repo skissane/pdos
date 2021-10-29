3f1f6e4437910c25de397b55eac1abb0dbb5cf0b
Paul Edwards <mutazilah@gmail.com>
2021-10-30 10:48:36 +1100

    more functions

--- generic/__os.h
+++ generic/__os.h
@@ -10,10 +10,25 @@
 /*                                                                   */
 /*********************************************************************/
 
+#ifndef __OS_INCLUDED
+#define __OS_INCLUDED
+
+#include <stddef.h>
+
 typedef struct {
     int (*__start)(char *p);
     int (*printf)(const char *format, ...);
     int (**main)(int argc, char **argv);
+    void *(*malloc)(size_t size);
+    void *Xstdin;
+    void *Xstdout;
+    void *(*fopen)(const char *filename, const char *mode);
+    int (*fseek)(void *stream, long offset, int whence);
+    size_t (*fread)(void *ptr, size_t size, size_t nmemb, void *stream);
+    int (*fclose)(void *stream);
+    size_t (*fwrite)(const void *ptr, size_t size, size_t nmemb, void *stream);
 } OS;
 
 extern OS *__os;
+
+#endif
