7abdd36143792eec6edea808108cca809161da61
Paul Edwards <mutazilah@gmail.com>
2021-10-29 23:15:59 +1100

    export more functions

--- generic/bios.h
+++ generic/bios.h
@@ -10,6 +10,14 @@
 /*                                                                   */
 /*********************************************************************/
 
+#include <stddef.h>
+
 typedef struct {
     int (*printf)(const char *format, ...);
+    void *(*fopen)(const char *filename, const char *mode);
+    int (*fseek)(void *stream, long offset, int whence);
+    size_t (*fread)(void *ptr, size_t size, size_t nmemb, void *stream);
+    int (*fclose)(void *stream);
+    size_t (*fwrite)(const void *ptr, size_t size, size_t nmemb, void *stream);
+    void *Xstdout;
 } BIOS;
