2518b9fa3201b16ab41dcb69bac23e5309c57abf
Paul Edwards <mutazilah@gmail.com>
2021-10-30 07:48:17 +1100

    startup routine for a PDOS-generic application

---
+++ pdpclib/pgastart.c
@@ -0,0 +1,28 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  pgastart - startup routine for PDOS generic applications         */
+/*                                                                   */
+/*********************************************************************/
+
+#include <__os.h>
+
+OS *__os;
+
+int main(int argc, char **argv);
+
+/* This name is known to certain versions of "ld" as the entry
+point of an application and there is no particular reason to not
+use it. */
+
+int __crt0(OS *os)
+{
+    __os = os;
+    *__os->main = main;
+    return (__os->__start(0));
+}
