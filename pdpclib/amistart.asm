f15a38222ce60eef848777ab31d04f60d0af3b52
Paul Edwards <mutazilah@gmail.com>
2021-11-07 21:48:39 +1100

    switch from mot to std

--- pdpclib/amistart.asm
+++ pdpclib/amistart.asm
@@ -1,35 +1,33 @@
-***********************************************************************
-*                                                                     *
-*  This program written by Paul Edwards.                              *
-*  Released to the public domain                                      *
-*                                                                     *
-***********************************************************************
-***********************************************************************
-*                                                                     *
-*  amistart - startup code for AmigaOS.                               *
-*                                                                     *
-*  This uses the Motorola syntax                                      *
-*                                                                     *
-*  It puts the standard AmigaOS registers containing the command      *
-*  length (d0) and command buffer (a0) as well as the AmigaPDOS       *
-*  extension register (only visible if running AmigaPDOS) d7,         *
-*  containing an alternative SysBase to use (only if d0 is greater    *
-*  than or equal to 2 GiB, and in which case, 2 GiB should be         *
-*  subtracted from d0 before use) on the stack.                       *
-*                                                                     *
-*  All this manipulation is left for the C startup code to handle.    *
-*                                                                     *
-***********************************************************************
-*
-        section "CODE",code
-        xref ___start
-        xdef ___amistart
+#######################################################################
+#                                                                     #
+#  This program written by Paul Edwards.                              #
+#  Released to the public domain                                      #
+#                                                                     #
+#######################################################################
+#######################################################################
+#                                                                     #
+#  amistart - startup code for AmigaOS.                               #
+#                                                                     #
+#  This uses the Motorola syntax                                      #
+#                                                                     #
+#  It puts the standard AmigaOS registers containing the command      #
+#  length (d0) and command buffer (a0) as well as the AmigaPDOS       #
+#  extension register (only visible if running AmigaPDOS) d7,         #
+#  containing an alternative SysBase to use (only if d0 is greater    #
+#  than or equal to 2 GiB, and in which case, 2 GiB should be         #
+#  subtracted from d0 before use) on the stack.                       #
+#                                                                     #
+#  All this manipulation is left for the C startup code to handle.    #
+#                                                                     #
+#######################################################################
 
+        .text
+        .globl ___amistart
 ___amistart:
         move.l  sp,savedSP
 
-* Don't use movem to push the arguments because the order
-* is not what you code yourself, but other rules.
+# Don't use movem to push the arguments because the order
+# is not what you code yourself, but other rules.
         move.l  d7,-(sp)
         move.l  a0,-(sp)
         move.l  d0,-(sp)
@@ -38,16 +36,16 @@ ___amistart:
         lea     12(sp),sp
         rts
 
-* This function receives a return code as a parameter. The stack
-* then needs to be restored and the parameter placed in register d0
-* prior to return to the OS.
+# This function receives a return code as a parameter. The stack
+# then needs to be restored and the parameter placed in register d0
+# prior to return to the OS.
 
-        xdef ___exita
+        .globl ___exita
 ___exita:
         move.l  4(sp),d0
         move.l  savedSP,sp
         rts
 
-        section "BSS",bss
+        .bss
 savedSP:
-        ds.l    1
+        .skip 4
