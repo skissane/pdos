26c5b9f60a7bc99e8729f83a10a586d48f3536af
Paul Edwards <mutazilah@gmail.com>
2022-01-09 17:56:15 +1100

    startup code for statically-linked win32 programs

---
+++ pdpclib/p32start.c
@@ -0,0 +1,34 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  p32start - startup code for non-MSVCRT WIN32                     */
+/*                                                                   */
+/*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/* This is the main entry point of a console mode executable */
+
+/* consider adding this line so that relocatables are generated
+   for some/most/all versions of "ld". The reason for this is
+   that, at least with binutils 2.14a, there is code in there
+   that will only generate relocatables if there are some
+   exported symbols (or some other conditions) */
+/*__declspec(dllexport)*/
+void mainCRTStartup(void)
+{
+    __start(0);
+    return;
+}
+
+void __main(void)
+{
+    return;
+}
