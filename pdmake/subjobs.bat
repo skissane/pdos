91f7c50b2ff4fcd48b13305f55274616111dce33
Paul Edwards <mutazilah@gmail.com>
2022-01-10 18:55:43 +1100

    batch files to build on MVS

---
+++ pdmake/subjobs.bat
@@ -0,0 +1,9 @@
+del alljcl.jcl
+type pdmake1.jcl >>alljcl.jcl
+type pdmake2.jcl >>alljcl.jcl
+type pdmake3.jcl >>alljcl.jcl
+type pdmake4.jcl >>alljcl.jcl
+type pdmake5.jcl >>alljcl.jcl
+type pdmake6.jcl >>alljcl.jcl
+call runmvs alljcl.jcl output.txt all.zip
+ 
---
+++ pdmake/zipmvs.bat
@@ -0,0 +1,10 @@
+del pminc.zip
+del pmsrc.zip
+del pmjcl.zip
+del all.zip
+del alljcl.jcl
+del output.txt
+zip -9 -X -ll -j pmsrc.zip *.txt *.c
+zip -9 -X -ll -j pminc.zip *.h
+zip -9 -X -ll -j pmjcl.zip *.jcl
+zip -9 -X all *.zip
