cf55274c972df7e9c3dec66961b16cd810e841f3
Paul Edwards <mutazilah@gmail.com>
2022-02-16 09:21:13 +1100

    switch to simplified syntax as per winsupc

--- pdpclib/winsupa.asm
+++ pdpclib/winsupa.asm
@@ -4,81 +4,67 @@
 ; Released to the public domain
 
 .386
+.model flat
 
-.model flat,c
+.code
 
-_DATA   segment dword 'DATA'
-_DATA   ends
-_BSS    segment dword 'BSS'
-_BSS    ends
+public ___setj
+public ___longj
+public ___chkstk_ms
 
-_TEXT segment 'CODE'
-
-
-public __setj
-__setj proc env:dword
-        mov eax, env
+___setj:
+        mov eax, [esp+4]
         push ebx
         mov ebx, esp
-        push ebx               ; esp
+        mov [eax+20], ebx    ; esp
 
-        mov [eax + 4], ecx
-        mov [eax + 8], edx
-        mov [eax + 12], edi
-        mov [eax + 16], esi
+        mov ebx, ebp
+        mov [eax+24], ebx    ; ebp
 
-        pop ebx
-        mov [eax + 20], ebx    ; esp
-        mov ebx, [ebp + 0]
-        mov [eax + 24], ebx    ; ebp
+        mov [eax+4], ecx
+        mov [eax+8], edx
+        mov [eax+12], edi
+        mov [eax+16], esi
 
-        mov ebx, [ebp + 4]     ; return address
-        mov [eax + 28], ebx    ; return address
+        mov ebx, [esp+4]   ; return address
+        mov [eax+28], ebx    ; return address
 
         pop ebx
-        mov [eax + 0], ebx
+        mov [eax+0], ebx
         mov eax, 0
 
         ret
-__setj endp
 
 
-public __longj
-__longj proc env:dword
-        mov eax, env
-        mov ebp, [eax + 20]
+___longj:
+        mov eax, [esp+4]
+        mov ebp, [eax+20]
         mov esp, ebp
+
         pop ebx                ; position of old ebx
-        pop ebx                ; position of old ebp
         pop ebx                ; position of old return address
 
-        mov ebx, [eax + 28]    ; return address
+        mov ebx, [eax+28]    ; return address
         push ebx
 
-        mov ebx, [eax + 24]    ; ebp saved as normal
-        push ebx
-        mov ebp, esp
+        mov ebx, [eax+24]
+        mov ebp, ebx
 
-        mov ebx, [eax + 0]
-        mov ecx, [eax + 4]
-        mov edx, [eax + 8]
-        mov edi, [eax + 12]
-        mov esi, [eax + 16]
+        mov ebx, [eax+0]
+        mov ecx, [eax+4]
+        mov edx, [eax+8]
+        mov edi, [eax+12]
+        mov esi, [eax+16]
 
-        mov eax, [eax + 32]    ; return value
+        mov eax, [eax+32]    ; return value
 
         ret
-__longj endp
 
 
 ; For compiling with GCC 4 we don't want to
 ; have to link the GCC library in
-public __chkstk_ms
-__chkstk_ms proc
+___chkstk_ms:
         ret
-__chkstk_ms endp
 
-
-_TEXT ends
 
 end
