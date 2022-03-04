e75fdc8479da1ba83297b4be13ebd8b0a95ff855
Paul Edwards <mutazilah@gmail.com>
2022-03-04 15:53:09 +1100

    set proper return code

--- src/fninit.s
+++ src/fninit.s
@@ -6,5 +6,10 @@
         .text
 start:
         fninit
+        mov $0,%al
         mov $0x4c,%ah
         int $0x21
+/ This instruction should not be needed, but there seems
+/ to be a bug in ld386 that makes the executable do an
+/ int 0 instead of int 21H
+        nop
