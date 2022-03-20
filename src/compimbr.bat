9bd59a7f5b01b00cc4d4124db62d30f39ca816da
Paul Edwards <mutazilah@gmail.com>
2022-03-20 13:53:23 +1100

    build 32-bit version

--- src/compimbr.bat
+++ src/compimbr.bat
@@ -1,5 +1,18 @@
 rem uses PDPCLIB's makefile.wcd
 
-wasm -zq -zcm -Dmemodel=large support.asm
-wcl -q -w -c -I. -ml -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib instmbr.c bos.c
-wlink File instmbr.obj,bos.obj,support.obj Name instmbr.exe Form dos Library ..\pdpclib\watcom.lib Option quiet
+rem wasm -zq -zcm -Dmemodel=large support.asm
+rem wcl -q -w -c -I. -ml -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib instmbr.c bos.c
+rem wlink File instmbr.obj,bos.obj,support.obj Name instmbr.exe Form dos Library ..\pdpclib\watcom.lib Option quiet
+
+
+gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__WIN32__ instmbr.c
+aswin -o instmbr.o instmbr.s
+del instmbr.s
+
+gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__32BIT__ bos.c
+aswin -o bos.o bos.s
+del bos.s
+
+aswin -o support.o support.s
+
+ldwin -o instmbr.exe ../pdpclib/w32start.o instmbr.o bos.o support.o ../pdpclib/msvcrt.a
