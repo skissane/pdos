0bf5035b34f776f9430da0f744076a65ba045dfd
Paul Edwards <mutazilah@gmail.com>
2022-03-24 11:48:10 +1100

    build using windows tools

--- src/compb.bat
+++ src/compb.bat
@@ -1,6 +1,9 @@
 @echo off
-wasmr -zq -zcm -Dmemodel=tiny pbootsec.asm
-rem tasm requires 2 passes to generate the desired near jump
-rem for first instruction
-rem tasm -m2 -Dmemodel=tiny pbootsec.asm
-tlink -t -x pbootsec,pbootsec.com,,,
+rem wasmr -zq -zcm -Dmemodel=tiny pbootsec.asm
+rem rem tasm requires 2 passes to generate the desired near jump
+rem rem for first instruction
+rem rem tasm -m2 -Dmemodel=tiny pbootsec.asm
+rem tlink -t -x pbootsec,pbootsec.com,,,
+
+wasm -zq -zcm -Dmemodel=tiny pbootsec.asm
+wlink File pbootsec.obj Name pbootsec.com Form dos com Option quiet
