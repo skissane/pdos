4ff70332f40e671b73eb7c6d4277cf048f395539
Paul Edwards <mutazilah@gmail.com>
2022-03-30 19:42:52 +1100

    add atoi etc

--- src/format.c
+++ src/format.c
@@ -906,3 +906,124 @@ static void dblcvt(double num, char cnvtype, size_t nwidth,
     strcat(result,work);
     return;
 }
+
+long int strtol(const char *nptr, char **endptr, int base);
+
+int atoi(const char *nptr)
+{
+    return ((int)strtol(nptr, (char **)NULL, 10));
+}
+
+long int atol(const char *nptr)
+{
+    return (strtol(nptr, (char **)NULL, 10));
+}
+
+/* this logic is also in vvscanf - if you update this, update
+   that one too */
+
+unsigned long int strtoul(
+    const char *nptr, char **endptr, int base)
+{
+    unsigned long x = 0;
+    int undecided = 0;
+
+    if (base == 0)
+    {
+        undecided = 1;
+    }
+    while (isspace((unsigned char)*nptr))
+    {
+        nptr++;
+    }
+    while (1)
+    {
+        if (isdigit((unsigned char)*nptr))
+        {
+            if (base == 0)
+            {
+                if (*nptr == '0')
+                {
+                    base = 8;
+                }
+                else
+                {
+                    base = 10;
+                    undecided = 0;
+                }
+            }
+            x = x * base + (*nptr - '0');
+            nptr++;
+        }
+        else if (isalpha((unsigned char)*nptr))
+        {
+            if ((*nptr == 'X') || (*nptr == 'x'))
+            {
+                if ((base == 0) || ((base == 8) && undecided))
+                {
+                    base = 16;
+                    undecided = 0;
+                    nptr++;
+                }
+                else if (base == 16)
+                {
+                    /* hex values are allowed to have an optional 0x */
+                    nptr++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else if (base <= 10)
+            {
+                break;
+            }
+            else
+            {
+                x = x * base + (toupper((unsigned char)*nptr) - 'A') + 10;
+                nptr++;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (endptr != NULL)
+    {
+        *endptr = (char *)nptr;
+    }
+    return (x);
+}
+
+long int strtol(const char *nptr, char **endptr, int base)
+{
+    unsigned long y;
+    long x;
+    int neg = 0;
+
+    while (isspace((unsigned char)*nptr))
+    {
+        nptr++;
+    }
+    if (*nptr == '-')
+    {
+        neg = 1;
+        nptr++;
+    }
+    else if (*nptr == '+')
+    {
+        nptr++;
+    }
+    y = strtoul(nptr, endptr, base);
+    if (neg)
+    {
+        x = (long)-y;
+    }
+    else
+    {
+        x = (long)y;
+    }
+    return (x);
+}
