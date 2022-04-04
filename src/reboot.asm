dafa72437447bd9a7abd1fbb52ecc52e3f035534
Paul Edwards <mutazilah@gmail.com>
2022-04-04 21:41:32 +1000

    reboot computer running MSDOS

---
+++ src/reboot.asm
@@ -0,0 +1,29 @@
+; Released to the public domain Matthew Parker and Paul Edwards
+; For different models just change the .model directive
+
+; reboot - reboot computer
+; works on MSDOS and clones
+
+% .model memodel, c
+
+public reboot
+
+        .code
+
+reboot proc
+        push es
+        mov ax, 040h
+        mov es, ax
+        mov word ptr es:[072h], 01234h
+        
+; we want to do a jmp 0ffffh:0000h, but that seems not to assemble
+        mov ax, 0ffffh
+        push ax
+        mov ax, 0
+        push ax
+        retf
+        pop es ; shouldn't get here
+        ret
+reboot endp
+
+end
