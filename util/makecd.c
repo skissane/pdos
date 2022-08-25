49ed544199de1ab0ff6c906fab9d2a3d3e1dd294
Paul Edwards <mutazilah@gmail.com>
2022-08-25 16:44:25 +0800

    support making a hard disk

--- util/makecd.c
+++ util/makecd.c
@@ -6,7 +6,7 @@
 /*********************************************************************/
 /*********************************************************************/
 /*                                                                   */
-/*  makecd - make a CD from a floppy image                           */
+/*  makecd - make a CD from a floppy or hard disk image              */
 /*                                                                   */
 /*  This program creates a bootable ISO                              */
 /*                                                                   */
@@ -24,23 +24,37 @@ int main(int argc, char **argv)
     int x;
     char *p;
     unsigned int checksum;
-    
-    if (argc <= 2)
+    int floppy;
+
+    if (argc <= 3)
     {
-        printf("usage: makecd <floppy image> <iso image>\n");
-        printf("example: makecd floppy.img out.iso\n");
+        printf("usage: makecd <fd/hd> <floppy/hard disk image> <iso image>\n");
+        printf("example: makecd fd floppy.img out.iso\n");
         return (EXIT_FAILURE);
     }
-    fp = fopen(*(argv + 1), "rb");
+    if (strcmp(*(argv + 1), "fd") == 0)
+    {
+        floppy = 1;
+    }
+    else if (strcmp(*(argv + 1), "hd") == 0)
+    {
+        floppy = 0;
+    }
+    else
+    {
+        printf("must specify either fd (floppy) or hd (hard disk)\n");
+        return (EXIT_FAILURE);
+    }
+    fp = fopen(*(argv + 2), "rb");
     if (fp == NULL)
     {
-        printf("failed to open %s for reading\n", *(argv + 1));
+        printf("failed to open %s for reading\n", *(argv + 2));
         return (EXIT_FAILURE);
     }
-    fq = fopen(*(argv + 2), "wb");
+    fq = fopen(*(argv + 3), "wb");
     if (fq == NULL)
     {
-        printf("failed to open %s for writing\n", *(argv + 2));
+        printf("failed to open %s for writing\n", *(argv + 3));
         return (EXIT_FAILURE);
     }
     /* 16 sectors are unused */
@@ -99,8 +113,16 @@ int main(int argc, char **argv)
     p[1] = 2; /* 1.44 MB floppy */
         /* note - 4 = hard disk, 0 = no emulation */
         /* 3 = 2.88 MB floppy, 1 = 1.2 MB floppy */
+    if (!floppy)
+    {
+        p[1] = 4;
+    }
     p[4] = 0; /* system type from partition table - not sure
            what a floppy requires */
+    if (!floppy)
+    {
+        p[4] = 0x0b; /* should probably read this from the image */
+    }
     p[6] = 1; /* load 1 512-byte sector */
     p[7] = 0; /* high byte */
     p[8] = 20;
@@ -110,9 +132,11 @@ int main(int argc, char **argv)
     fwrite(buf, sizeof buf, 1, fq);
 
     /* not sure if we can start our floppy image now */
-    while (fread(buf, sizeof buf, 1, fp) == 1)
+    memset(buf, '\0', sizeof buf);
+    while (fread(buf, 1, sizeof buf, fp) != 0)
     {
         fwrite(buf, sizeof buf, 1, fq);
+        memset(buf, '\0', sizeof buf);
     }
     if (ferror(fq))
     {
