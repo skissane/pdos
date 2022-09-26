e917f6fd79f041a7cc900a420d2e7c7e7d5cd135
Paul Edwards <mutazilah@gmail.com>
2022-09-27 07:25:47 +0800

    switch off liballoc

--- src/compk32.bat
+++ src/compk32.bat
@@ -1,29 +1,29 @@
-gccwin -S -Os -D__WIN32__ -D__EXPORT__ -I ../pdpclib -I . ../pdpclib/dllcrt.c
+gccwin -S -O2 -ansi -DNOLIBALLOC -D__WIN32__ -D__EXPORT__ -I ../pdpclib -I . ../pdpclib/dllcrt.c
 aswin -o dllcrt.o dllcrt.s
 del dllcrt.s
 
-gccwin -S -Os -D__WIN32__ -D__EXPORT__ -I ../pdpclib -I . ../pdpclib/string.c
+gccwin -S -O2 -ansi -DNOLIBALLOC -D__WIN32__ -D__EXPORT__ -I ../pdpclib -I . ../pdpclib/string.c
 aswin -o string.o string.s
 del string.s
 
-gccwin -S -Os -D__WIN32__ -D__EXPORT__ -I ../pdpclib -I . kernel32.c
+gccwin -S -O2 -ansi -DNOLIBALLOC -D__WIN32__ -D__EXPORT__ -I ../pdpclib -I . kernel32.c
 aswin -o kernel32.o kernel32.s
 del kernel32.s
 
-gccwin -S -Os -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . pos.c
+gccwin -S -O2 -ansi -DNOLIBALLOC -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . pos.c
 aswin -o pos.o pos.s
 del pos.s
 
 aswin -o support.o support.s
 
-gccwin -S -Os -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . ../pdpclib/liballoc.c
+gccwin -S -O2 -ansi -DNOLIBALLOC -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . ../pdpclib/liballoc.c
 aswin -o liballoc.o liballoc.s
 del liballoc.s
 
-gccwin -S -Os -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . ../pdpclib/pdossupc.c
+gccwin -S -O2 -ansi -DNOLIBALLOC -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . ../pdpclib/pdossupc.c
 aswin -o pdossupc.o pdossupc.s
 del pdossupc.s
 
-ldwin -s -o kernel32.dll --shared --kill-at dllcrt.o kernel32.o pos.o support.o string.o liballoc.o pdossupc.o
+ldwin -s -o kernel32.dll --shared --kill-at dllcrt.o kernel32.o pos.o support.o string.o pdossupc.o
 
 dlltwin -S aswin -k --export-all-symbols -D kernel32.dll kernel32.o -l kernel32.a
