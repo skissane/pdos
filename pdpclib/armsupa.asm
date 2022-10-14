a20fa487faf8c59f2d1f2698616d1f95f33ae513
Paul Edwards <mutazilah@gmail.com>
2022-10-15 02:17:22 +0800

    more efficient return from main

--- pdpclib/armsupa.asm
+++ pdpclib/armsupa.asm
@@ -170,10 +170,7 @@ timok:  ldr     r0,[sp]
 # This function is required by GCC but isn't used for anything
         .globl ___main
 ___main:
-	mov	ip, sp
-	stmfd	sp!, {fp, ip, lr, pc}
-	sub	fp, ip, #4
-	ldmea	fp, {fp, sp, pc}^
+        movs    pc, lr
 
 # unsigned integer divide
 # inner loop code taken from http://me.henri.net/fp-div.html
