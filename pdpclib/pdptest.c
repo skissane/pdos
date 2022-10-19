78dde936512ef0c022449e99a24bde69845c77f6
Paul Edwards <mutazilah@gmail.com>
2022-10-19 14:07:57 +0800

    close input file

--- pdpclib/pdptest.c
+++ pdpclib/pdptest.c
@@ -150,7 +150,8 @@ int main(int argc, char **argv)
         return (EXIT_FAILURE);
     }
     printf("%lu bytes copied\n", total);
-    
+
+    fclose(fp);
     fclose(fq); /* keep last in case it is stdout */
 
     return (0);
