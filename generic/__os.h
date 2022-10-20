bb86f97b67b2a68f0d29df9c4511d9e71d198802
Paul Edwards <mutazilah@gmail.com>
2022-10-20 16:47:00 +0800

    add strcpy and strlen

--- generic/__os.h
+++ generic/__os.h
@@ -31,6 +31,8 @@ typedef struct {
     char *(*strchr)(const char *s, int c);
     int (*strcmp)(const char *s1, const char *s2);
     int (*strncmp)(const char *s1, const char *s2, size_t n);
+    char (*strcpy)(char *s1, const char *s2);
+    size_t (*strlen)(const char *s);
     int (*fgetc)(void *stream);
     int (*fputc)(int c, void *stream);
     void *(*PosGetDTA)(void);
