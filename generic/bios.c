20b4239f1281b9221a3def2a81dc57e949eb0b1b
Paul Edwards <mutazilah@gmail.com>
2022-10-31 23:35:48 +0800

    add directory capability for Linux

--- generic/bios.c
+++ generic/bios.c
@@ -42,11 +42,27 @@ int their_start(char *parm);
 static int getmainargs(int *_Argc,
                        char ***_Argv);
 
+#if defined(__gnu_linux__) || defined(__ARM__)
+#include <pos.h>
+
+static int dirfile;
+
+static DTA origdta;
+
+void *PosGetDTA(void);
+int PosFindFirst(char *pat, int attrib);
+int PosFindNext(void);
+#endif
+
 static OS bios = { their_start, 0, 0, NULL, printf, 0, malloc, NULL, NULL,
   fopen, fseek, fread, fclose, fwrite, fgets, strchr,
   strcmp, strncmp, strcpy, strlen, fgetc, fputc,
   fflush, setvbuf,
+#if defined(__gnu_linux__) || defined(__ARM__)
+  PosGetDTA, PosFindFirst, PosFindNext,
+#else
   0, 0, 0,
+#endif
   0, 0 };
 
 static char buf[200];
@@ -214,3 +230,49 @@ static int getmainargs(int *_Argc,
     *_Argv = argv;
     return (0);
 }
+
+#if defined(__gnu_linux__) || defined(__ARM__)
+
+void *PosGetDTA(void)
+{
+    return (&origdta);
+}
+
+static int ff_search(void)
+{
+    static unsigned char buf[500];
+    static size_t upto = 0;
+    static size_t avail = 0;
+
+    if (avail <= 0)
+    {
+        avail = __getdents(dirfile, buf, 500);
+        if (avail <= 0)
+        {
+            __close(dirfile);
+            return (1);
+        }
+    }
+    strncpy(origdta.file_name, buf + upto + 10, 11);
+    origdta.file_name[11] = '\0';
+    upto += *(short *)(buf + upto + 8);
+    if (upto >= avail)
+    {
+        upto = avail = 0;
+    }
+    return (0);
+}
+
+int PosFindFirst(char *pat, int attrib)
+{
+    dirfile = __open(".", 0, 0);
+    if (dirfile < 0) return (1);
+    return (ff_search());
+}
+
+int PosFindNext(void)
+{
+    return (ff_search());
+}
+
+#endif
