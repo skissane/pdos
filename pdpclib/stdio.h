bc3f89311ed4b5f43a4c93c77f92deca76238e99
Paul Edwards <mutazilah@gmail.com>
2022-11-05 19:26:45 +0800

    eliminate warnings from compiling linux

--- pdpclib/stdio.h
+++ pdpclib/stdio.h
@@ -20,10 +20,10 @@
 #define __SIZE_T_DEFINED
 #if (defined(__OS2__) || defined(__32BIT__) || defined(__MVS__) \
     || defined(__CMS__) || defined(__VSE__) || defined(__SMALLERC__) \
-    || defined(__ARM__))
+    || defined(__ARM__) || defined(__gnu_linux__))
 typedef unsigned long size_t;
 #elif (defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC) \
-    || defined(__WIN32__) || defined(__gnu_linux__) || defined(__AMIGA__) \
+    || defined(__WIN32__) || defined(__AMIGA__) \
     || defined(__PDOS386__))
 typedef unsigned int size_t;
 #endif
