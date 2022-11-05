5b27e5b8fe46a2ca1426e77063d4cb4468237106
Paul Edwards <mutazilah@gmail.com>
2022-11-06 06:34:08 +0800

    allow as86 as a build option

--- src/compfni.bat
+++ src/compfni.bat
@@ -1,5 +1,8 @@
 rem wasm -zq -zcm fninit.asm
 rem wlink File fninit.obj Name fninit.exe Form dos Option quiet
 
+rem as86 -DAS86 -o fninit.o fninit.asm
+rem ld86 --oformat msdos-mz -o fninit.exe fninit.o
+
 as386 -o fninit.o fninit.s
 ld386 -s -N -e start -o fninit.exe fninit.o
