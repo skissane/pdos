2b296f0b0d46ac631b2b9f3f47c5e3f5cbe9d729
Paul Edwards <mutazilah@gmail.com>
2022-11-06 08:35:13 +0800

    cleaner DOS implementation

--- src/compfni.bat
+++ src/compfni.bat
@@ -1,8 +1,9 @@
+rem rem ml /c /nologo fninit.asm
 rem wasm -zq -zcm fninit.asm
-rem wlink File fninit.obj Name fninit.exe Form dos Option quiet
+rem wlink File fninit.obj Name fninit.exe Form dos Option quiet,start=start
 
-rem as86 -DAS86 -o fninit.o fninit.asm
-rem ld86 --oformat msdos-mz -o fninit.exe fninit.o
+rem as86 -o fninit.o fninit.asm
+rem ld86 --oformat msdos-mz -e start -o fninit.exe fninit.o
 
 as386 -o fninit.o fninit.s
 ld386 -s -N -e start -o fninit.exe fninit.o
--- src/fninit.asm
+++ src/fninit.asm
@@ -3,19 +3,18 @@
 ; This program written by Paul Edwards
 ; Released to the public domain
 
-ifndef AS86
 .model tiny
 
 .stack 400h
 
 .code
-endif
 
-top:
+public start
+start:
 
 fninit
 mov al, 0
 mov ah, 4ch
 int 21h
 
-end top
+end
