e023530232b325b6f144dcef9747019c33623993
Paul Edwards <mutazilah@gmail.com>
2022-11-06 08:39:11 +0800

    suppress warnings from as86

--- src/compfni.bat
+++ src/compfni.bat
@@ -2,7 +2,7 @@ rem rem ml /c /nologo fninit.asm
 rem wasm -zq -zcm fninit.asm
 rem wlink File fninit.obj Name fninit.exe Form dos Option quiet,start=start
 
-rem as86 -o fninit.o fninit.asm
+rem as86 --nowarn -o fninit.o fninit.asm
 rem ld86 --oformat msdos-mz -e start -o fninit.exe fninit.o
 
 as386 -o fninit.o fninit.s
