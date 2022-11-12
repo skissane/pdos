8d7acce51f15654cb0304cd419b61ec1aca9ad34
Paul Edwards <mutazilah@gmail.com>
2022-11-12 12:31:35 +0800

    startup code for EFI applications

---
+++ pdpclib/efistart.c
@@ -0,0 +1,20 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*  startup code for EFI applications                                */
+/*                                                                   */
+/*********************************************************************/
+
+static int __main(void);
+
+int main(void)
+{
+    return (5);
+}
+
+static int __main(void)
+{
+    return (3);
+}
