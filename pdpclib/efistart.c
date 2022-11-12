e353589b9b61dd1b2184167af4ac06355e99022a
Paul Edwards <mutazilah@gmail.com>
2022-11-12 22:07:58 +0800

    start adding placeholder functionality

--- pdpclib/efistart.c
+++ pdpclib/efistart.c
@@ -7,10 +7,26 @@
 /*                                                                   */
 /*********************************************************************/
 
+typedef struct {
+    char junk1[50];
+    int (*print_func)(void *x);
+} EFI_SIMPLE_TEXT;
+
+typedef struct {
+    char junk1[50];
+    int stdout_handle;
+    EFI_SIMPLE_TEXT *simple;
+} EFI_SYSTEM;
+
+static char onechar[3];
+
 static int __main(void);
 
-int main(void)
+int main(int junk, EFI_SYSTEM *system)
 {
+    onechar[0] = 'X';
+    system->simple->print_func(onechar);
+    for (;;) ;
     return (5);
 }
 
