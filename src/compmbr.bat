f56bf51f1e26c76cf0f1334574d24b75697e70ed
Paul Edwards <mutazilah@gmail.com>
2022-11-14 20:21:14 +0800

    warn about watcom 1.6 misbehavior

--- src/compmbr.bat
+++ src/compmbr.bat
@@ -1,5 +1,7 @@
 rem wasmr -zq -zcm -Dmemodel=tiny mbr.asm
 rem tasm -m2 -Dmemodel=tiny mbr.asm
 rem tlink -t -x mbr,mbr.com,,,
+
+rem wasm is incorrectly treating warnings as errors
 wasm -zq -zcm -Dmemodel=tiny mbr.asm
 wlink File mbr.obj Name mbr.com Form dos com Option quiet
