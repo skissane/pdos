f6cdbe49edd568fb304240f25a0feeae402b7e11
Paul Edwards <mutazilah@gmail.com>
2022-11-21 21:09:06 +0800

    add other build methods

--- src/compbf32.bat
+++ src/compbf32.bat
@@ -1,5 +1,12 @@
 rem wasmr -zq -zcm -Dmemodel=tiny fat32.asm
 rem tasm -m2 -Dmemodel=tiny fat32.asm
 rem tlink -t -x fat32,fat32.com,,,
+
 wasm -zq -zcm -Dmemodel=tiny fat32.asm
 wlink File fat32.obj Name fat32.com Form dos com Option quiet
+
+rem as86 -Dmemodel=tiny -o fat32.o fat32.asm
+rem ld86 -o fat32.com --oformat msdos fat32.o
+
+rem ml -c -Dmemodel=tiny fat32.asm
+rem wlink File fat32.obj Name fat32.com Form dos com Option quiet
