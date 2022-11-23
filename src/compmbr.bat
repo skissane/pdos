2760569237d4e88d9ed57da48a4e20bb5e0e780f
Paul Edwards <mutazilah@gmail.com>
2022-11-23 14:56:10 +0800

    switch to as86

--- src/compmbr.bat
+++ src/compmbr.bat
@@ -4,8 +4,10 @@ rem tlink -t -x mbr,mbr.com,,,
 
 rem wasm is incorrectly treating warnings as errors
 rem so be careful if you see any warnings
-wasm -zq -zcm -Dmemodel=tiny mbr.asm
-wlink File mbr.obj Name mbr.com Form dos com Option quiet
+rem wasm -zq -zcm -Dmemodel=tiny mbr.asm
+rem wlink File mbr.obj Name mbr.com Form dos com Option quiet
 
-rem as86 -Dmemodel=tiny -o mbr.o mbr.asm
-rem ld86 -o mbr.com --oformat msdos mbr.o
+as86 -Dmemodel=tiny -o mbr.o mbr.asm
+rem need to use binary format because ld86 will pad a com
+rem to a 16-byte boundary
+ld86 -o mbr.com --oformat binary mbr.o
