4ca11c98043da886ce9d80a31590ee5b5389b475
Paul Edwards <mutazilah@gmail.com>
2022-11-23 13:37:03 +0800

    prepare for as86

--- src/compmbr.bat
+++ src/compmbr.bat
@@ -6,3 +6,6 @@ rem wasm is incorrectly treating warnings as errors
 rem so be careful if you see any warnings
 wasm -zq -zcm -Dmemodel=tiny mbr.asm
 wlink File mbr.obj Name mbr.com Form dos com Option quiet
+
+rem as86 -Dmemodel=tiny -o mbr.o mbr.asm
+rem ld86 -o mbr.com --oformat msdos mbr.o
