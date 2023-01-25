3d67f497e7d00344559bf4705f53feb1fe5dd43d
Paul Edwards <mutazilah@gmail.com>
2023-01-25 20:09:21 +0800

    bug fix for determining number of bytes needed

--- s370/mkiplcrd.c
+++ s370/mkiplcrd.c
@@ -113,6 +113,7 @@ int main(int argc, char **argv)
                    of usable data */
                 rem = higherlen % (9 * 72);
                 q = higherlen / (9 * 72);
+                q *= 72;
                 if (rem != 0)
                 {
                     /* we are dealing with bytes, and we need another
