9a65963e8755030adc7b26876464c657276da2fa
Paul Edwards <mutazilah@gmail.com>
2023-01-31 12:34:53 +0800

    convert keyboard input to EBCDIC

--- util/mfterm.c
+++ util/mfterm.c
@@ -81,6 +81,7 @@ static void interact(FILE *sf);
 static void expect(FILE *sf, unsigned char *buf, size_t buflen);
 
 int febc(int ebc);
+int tebc(int local);
 
 int main(int argc, char **argv)
 {
@@ -300,16 +301,22 @@ static void interact(FILE *sf)
             }
             if (termtype == 3270)
             {
-                char *p;
+                unsigned char *p;
                 char fixed[6] = "\x7d\x5b\xe2\x11\x5b\x61";
 
                 printf("type something\n");
                 fgets(keybbuf, sizeof keybbuf, stdin);
-                printf("you typed x%sx\n", keybbuf);
+                /* printf("you typed x%sx\n", keybbuf); */
                 p = strchr(keybbuf, '\n');
                 if (p != NULL)
                 {
                     *p = '\0';
+                }
+                p = keybbuf;
+                while (*p != '\0')
+                {
+                    *p = tebc(*p);
+                    p++;
                 }
                 /* I need 6 characters */
                 /* that e2 changes */
@@ -317,7 +324,7 @@ static void interact(FILE *sf)
                 /* fixed[2] = 0xe1 + strlen(keybbuf); */
                 fwrite(fixed, 1, 6, sf);
                 fwrite(keybbuf, 1, strlen(keybbuf), sf);
-                /* i suspect i need this too */
+                /* testing revealed i need this too */
                 fwrite("\xff\xef", 1, 2, sf);
                 continue;
             }
@@ -477,5 +484,123 @@ int febc(int ebc)
     case 0xd0 : return('}');
     case 0xa1 : return('~');
     default   : return(0);
+  }
+}
+
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written By Paul Edwards.                            */
+/*  Released to the public domain.                                   */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  This program takes an integer as a parameter.  The integer       */
+/*  should have a character in the local character set.  It will be  */
+/*  converted into the EBCDIC equivalent.                            */
+/*                                                                   */
+/*********************************************************************/
+
+int tebc(int local)
+{
+  switch (local)
+  {
+    case '\n' : return (0x15);
+    case ' '  : return (0x40);
+    case '!'  : return (0x5A);
+    case '\"' : return (0x7f);
+    case '#'  : return (0x7b);
+    case '$'  : return (0x5b);
+    case '%'  : return (0x6c);
+    case '&'  : return (0x50);
+    case '\'' : return (0x7d);
+    case '('  : return (0x4d);
+    case ')'  : return (0x5d);
+    case '*'  : return (0x5c);
+    case '+'  : return (0x4e);
+    case ','  : return (0x6b);
+    case '-'  : return (0x60);
+    case '.'  : return (0x4b);
+    case '/'  : return (0x61);
+    case '0'  : return (0xf0);
+    case '1'  : return (0xf1);
+    case '2'  : return (0xf2);
+    case '3'  : return (0xf3);
+    case '4'  : return (0xf4);
+    case '5'  : return (0xf5);
+    case '6'  : return (0xf6);
+    case '7'  : return (0xf7);
+    case '8'  : return (0xf8);
+    case '9'  : return (0xf9);
+    case ':'  : return (0x7a);
+    case ';'  : return (0x5e);
+    case '<'  : return (0x4c);
+    case '='  : return (0x7e);
+    case '>'  : return (0x6e);
+    case '?'  : return (0x6f);
+    case '@'  : return (0x7c);
+    case 'A'  : return (0xc1);
+    case 'B'  : return (0xc2);
+    case 'C'  : return (0xc3);
+    case 'D'  : return (0xc4);
+    case 'E'  : return (0xc5);
+    case 'F'  : return (0xc6);
+    case 'G'  : return (0xc7);
+    case 'H'  : return (0xc8);
+    case 'I'  : return (0xc9);
+    case 'J'  : return (0xd1);
+    case 'K'  : return (0xd2);
+    case 'L'  : return (0xd3);
+    case 'M'  : return (0xd4);
+    case 'N'  : return (0xd5);
+    case 'O'  : return (0xd6);
+    case 'P'  : return (0xd7);
+    case 'Q'  : return (0xd8);
+    case 'R'  : return (0xd9);
+    case 'S'  : return (0xe2);
+    case 'T'  : return (0xe3);
+    case 'U'  : return (0xe4);
+    case 'V'  : return (0xe5);
+    case 'W'  : return (0xe6);
+    case 'X'  : return (0xe7);
+    case 'Y'  : return (0xe8);
+    case 'Z'  : return (0xe9);
+    case '['  : return (0xad);
+    case '\\' : return (0xe0);
+    case ']'  : return (0xbd);
+    case '^'  : return (0x5f);
+    case '_'  : return (0x6d);
+    case '`'  : return (0x79);
+    case 'a'  : return (0x81);
+    case 'b'  : return (0x82);
+    case 'c'  : return (0x83);
+    case 'd'  : return (0x84);
+    case 'e'  : return (0x85);
+    case 'f'  : return (0x86);
+    case 'g'  : return (0x87);
+    case 'h'  : return (0x88);
+    case 'i'  : return (0x89);
+    case 'j'  : return (0x91);
+    case 'k'  : return (0x92);
+    case 'l'  : return (0x93);
+    case 'm'  : return (0x94);
+    case 'n'  : return (0x95);
+    case 'o'  : return (0x96);
+    case 'p'  : return (0x97);
+    case 'q'  : return (0x98);
+    case 'r'  : return (0x99);
+    case 's'  : return (0xa2);
+    case 't'  : return (0xa3);
+    case 'u'  : return (0xa4);
+    case 'v'  : return (0xa5);
+    case 'w'  : return (0xa6);
+    case 'x'  : return (0xa7);
+    case 'y'  : return (0xa8);
+    case 'z'  : return (0xa9);
+    case '{'  : return (0xc0);
+    case '|'  : return (0x4f);
+    case '}'  : return (0xd0);
+    case '~'  : return (0xa1);
+    default   : return (0);
   }
 }
