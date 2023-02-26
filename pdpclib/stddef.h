3d81a9baf14b69a306fcb3a5829508ef6125bf0d
Paul Edwards <mutazilah@gmail.com>
2023-02-26 16:54:46 +0800

    support EFI

--- pdpclib/stddef.h
+++ pdpclib/stddef.h
@@ -22,7 +22,7 @@ typedef int ptrdiff_t;
     || defined(__SZ4__))
 typedef unsigned long size_t;
 #elif (defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC) \
-    || defined(__WIN32__) || defined(__AMIGA__))
+    || defined(__WIN32__) || defined(__AMIGA__) || defined(__EFI__))
 typedef unsigned int size_t;
 #endif
 #endif
