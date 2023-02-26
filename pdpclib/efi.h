a7d2e7daf037a895ff871d73fab4fdc157cb857c
Paul Edwards <mutazilah@gmail.com>
2023-02-26 12:58:10 +0800

    a lot more functionality available

--- pdpclib/efi.h
+++ pdpclib/efi.h
@@ -12,6 +12,7 @@ typedef unsigned char BOOLEAN;
 typedef int INTN;
 typedef unsigned int UINTN;
 
+typedef unsigned char UINT8;
 typedef unsigned short UINT16;
 
 typedef unsigned long UINT32;
@@ -24,24 +25,51 @@ typedef struct {
 typedef char CHAR8;
 typedef short CHAR16;
 
+typedef struct {
+    UINT32 a;
+    UINT16 b;
+    UINT16 c;
+    UINT8 d[8];
+} EFI_GUID;
+    
 typedef UINTN EFI_STATUS;
 typedef void * EFI_HANDLE;
 typedef void * EFI_EVENT;
 
+#define IN
+#define OUT
+#define OPTIONAL
+#define CONST
+#define EFIAPI
+
 #define EFI_SUCCESS 0
-
-
+/* Highest bit of EFI_STATUS set. */
+#define EFI_LOAD_ERROR 1
+#define EFI_INVALID_PARAMETER 2
+#define EFI_UNSUPPORTED 3
+#define EFI_BAD_BUFFER_SIZE 4
+#define EFI_BUFFER_TOO_SMALL 5
+#define EFI_NOT_READY 6
+#define EFI_DEVICE_ERROR 7
+#define EFI_WRITE_PROTECTED 8
+#define EFI_OUT_OF_RESOURCES 9
+#define EFI_VOLUME_CORRUPTED 10
+#define EFI_VOLUME_FULL 11
+#define EFI_NO_MEDIA 12
+#define EFI_MEDIA_CHANGED 13
+#define EFI_NOT_FOUND 14
+#define EFI_ACCESS_DENIED 15
 
 struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
 
-typedef EFI_STATUS (*EFI_INPUT_RESET) (struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, BOOLEAN ExtendedVerification);
+typedef EFI_STATUS (EFIAPI *EFI_INPUT_RESET) (IN struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, IN BOOLEAN ExtendedVerification);
 
 typedef struct {
     UINT16 ScanCode;
     CHAR16 UnicodeChar;
 } EFI_INPUT_KEY;
 
-typedef EFI_STATUS (*EFI_INPUT_READ_KEY) (struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, EFI_INPUT_KEY *Key);
+typedef EFI_STATUS (EFIAPI *EFI_INPUT_READ_KEY) (IN struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, OUT EFI_INPUT_KEY *Key);
 
 typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
     EFI_INPUT_RESET Reset;
@@ -52,7 +80,7 @@ typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
 
 struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-typedef EFI_STATUS (*EFI_TEXT_STRING) (struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_STRING) (IN struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN CHAR16 *String);
 
 typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
     void *Reset;
@@ -75,9 +103,52 @@ typedef struct {
     UINT32 Reserved;
 } EFI_TABLE_HEADER;
 
-typedef EFI_STATUS (*EFI_WAIT_FOR_EVENT) (UINTN NumberOfEvents, EFI_EVENT *Event, UINTN *Index);
+typedef enum {
+    EfiReservedMemoryType,
+    EfiLoaderCode,
+    EfiLoaderData, /* Should be used for most allocations. */
+    EfiBootServicesCode,
+    EfiBootServicesData,
+    EfiRuntimeServicesCode,
+    EfiRuntimeServicesData,
+    EfiConventionalMemory,
+    EfiUnusableMemory,
+    EfiACPIReclaimMemory,
+    EfiACPYMemoryNVS,
+    EfiMemoryMappedIO,
+    EfiMemoryMappedIOPortSpace,
+    EfiPalCode,
+    EfiPersistentMemory,
+    EfiUnacceptedMemoryType,
+    EfiMaxMemoryType
+} EFI_MEMORY_TYPE;
 
-typedef EFI_STATUS (*EFI_SET_WATCHDOG_TIMER) (UINTN Timeout, UINT64 WatchdogCode, UINTN DataSize, CHAR16 *WatchdogData);
+typedef EFI_STATUS (EFIAPI *EFI_ALLOCATE_POOL) (IN EFI_MEMORY_TYPE PoolType, IN UINTN Size, OUT void **Buffer);
+typedef EFI_STATUS (EFIAPI *EFI_FREE_POOL) (IN void *Buffer);
+
+typedef EFI_STATUS (EFIAPI *EFI_WAIT_FOR_EVENT) (IN UINTN NumberOfEvents, IN EFI_EVENT *Event, OUT UINTN *Index);
+
+typedef EFI_STATUS (EFIAPI *EFI_SET_WATCHDOG_TIMER) (IN UINTN Timeout, IN UINT64 WatchdogCode, IN UINTN DataSize, IN CHAR16 *WatchdogData);
+
+typedef EFI_STATUS (EFIAPI *EFI_HANDLE_PROTOCOL) (IN EFI_HANDLE Handle, IN EFI_GUID *Protocol, OUT void **Interface);
+
+#define EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL  0x00000001
+#define EFI_OPEN_PROTOCOL_GET_PROTOCOL        0x00000002
+#define EFI_OPEN_PROTOCOL_TEST_PROTOCOL       0x00000004
+#define EFI_OPEN_PROTOCOL_BY_CHILD_CONTROLLER 0x00000008
+#define EFI_OPEN_PROTOCOL_BY_DRIVER           0x00000010
+#define EFI_OPEN_PROTOCOL_EXCLUSIVE           0x00000020
+
+typedef EFI_STATUS (EFIAPI *EFI_OPEN_PROTOCOL) (IN EFI_HANDLE Handle,
+                                                IN EFI_GUID *Protocol,
+                                                OUT void **Interface OPTIONAL,
+                                                IN EFI_HANDLE AgentHandle,
+                                                IN EFI_HANDLE ControllerHandle,
+                                                IN UINT32 Attributes);
+typedef EFI_STATUS (EFIAPI *EFI_CLOSE_PROTOCOL) (IN EFI_HANDLE Handle,
+                                                 IN EFI_GUID *Protocol,
+                                                 IN EFI_HANDLE AgentHandle,
+                                                 IN EFI_HANDLE ControllerHandle);
 
 typedef struct {
     EFI_TABLE_HEADER Hdr;
@@ -88,8 +159,8 @@ typedef struct {
     void *AllocatePages;
     void *FreePages;
     void *GetMemoryMap;
-    void *AllocPool;
-    void *FreePool;
+    EFI_ALLOCATE_POOL AllocPool;
+    EFI_FREE_POOL FreePool;
 
     void *CreateEvent;
     void *SetTimer;
@@ -101,7 +172,7 @@ typedef struct {
     void *InstallProtocolInterface;
     void *ReinstallProtocolInterface;
     void *UninstallProtocolInterface;
-    void *HandleProtocol;
+    EFI_HANDLE_PROTOCOL HandleProtocol;
     void *RegisterProtocolNotify;
     void *LocateHandle;
     void *LocateDevicePath;
@@ -120,8 +191,8 @@ typedef struct {
     void *ConnectController;
     void *DisconnectController;
 
-    void *OpenProtocol;
-    void *CloseProtocol;
+    EFI_OPEN_PROTOCOL OpenProtocol; /* EFI 1.1+ */
+    EFI_CLOSE_PROTOCOL CloseProtocol; /* EFI 1.1+ */
     void *OpenProtocolInformation;
 
     void *ProtocolsPerHandle;
@@ -152,3 +223,103 @@ typedef struct {
     UINTN NumberOfTableEntries;
     void *ConfigurationTable;
 } EFI_SYSTEM_TABLE;
+
+typedef EFI_STATUS (EFIAPI *EFI_IMAGE_ENTRY_POINT) (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable);
+
+#define EFI_LOADED_IMAGE_PROTOCOL_GUID \
+ {0x5b1b31a1,0x9562,0x11d2,{0x8e,0x3f,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
+#define EFI_LOADED_IMAGE_PROTOCOL_REVISION 0x1000
+
+typedef struct {
+    UINT32 Revision;
+    EFI_HANDLE ParentHandle;
+    EFI_SYSTEM_TABLE *SystemTable;
+
+    /* Source location of the image. */
+    EFI_HANDLE DeviceHandle;
+    void *FilePath;
+    void *Reserved;
+
+    UINT32 LoadOptionsSize;
+    void *LoadOptions;
+
+    void *ImageBase;
+    UINT64 ImageSize;
+    EFI_MEMORY_TYPE ImageCodeType;
+    EFI_MEMORY_TYPE ImageDataType;
+    void *Unload;
+} EFI_LOADED_IMAGE_PROTOCOL;
+
+/* File system protocol. */
+#define EFI_FILE_PROTOCOL_REVISION  0x00010000
+#define EFI_FILE_PROTOCOL_REVISION2 0x00020000
+#define EFI_FILE_PROTOCOL_LATEST_REVISION EFI_FILE_PROTOCOL_REVISION2
+
+struct _EFI_FILE_PROTOCOL;
+
+#define EFI_FILE_MODE_READ   0x0000000000000001
+#define EFI_FILE_MODE_WRITE  0x0000000000000002
+#define EFI_FILE_MODE_CREATE 0x8000000000000000
+
+#define EFI_FILE_READ_ONLY  0x0000000000000001
+#define EFI_FILE_HIDDEN     0x0000000000000002
+#define EFI_FILE_SYSTEM     0x0000000000000004
+#define EFI_FILE_RESERVED   0x0000000000000008
+#define EFI_FILE_DIRECTORY  0x0000000000000010
+#define EFI_FILE_ARCHIVE    0x0000000000000020
+#define EFI_FILE_VALID_ATTR 0x0000000000000037
+
+typedef EFI_STATUS (EFIAPI *EFI_FILE_OPEN) (IN struct _EFI_FILE_PROTOCOL *This,
+                                            OUT struct _EFI_FILE_PROTOCOL **NewHandle,
+                                            IN CHAR16 *FileName,
+                                            IN UINT64 OpenMode,
+                                            IN UINT64 Attributes);
+typedef EFI_STATUS (EFIAPI *EFI_FILE_CLOSE) (IN struct _EFI_FILE_PROTOCOL *This);
+typedef EFI_STATUS (EFIAPI *EFI_FILE_DELETE) (IN struct _EFI_FILE_PROTOCOL *This);
+typedef EFI_STATUS (EFIAPI *EFI_FILE_READ) (IN struct _EFI_FILE_PROTOCOL *This,
+                                            IN OUT UINTN *BufferSize,
+                                            OUT void *Buffer);
+typedef EFI_STATUS (EFIAPI *EFI_FILE_WRITE) (IN struct _EFI_FILE_PROTOCOL *This,
+                                             IN OUT UINTN *BufferSize,
+                                             IN void *Buffer);
+typedef EFI_STATUS (EFIAPI *EFI_FILE_GET_POSITION) (IN struct _EFI_FILE_PROTOCOL *This,
+                                                    OUT UINT64 *Position);
+typedef EFI_STATUS (EFIAPI *EFI_FILE_SET_POSITION) (IN struct _EFI_FILE_PROTOCOL *This,
+                                                    IN UINT64 Position);
+
+
+typedef EFI_STATUS (EFIAPI *EFI_FILE_FLUSH) (IN struct _EFI_FILE_PROTOCOL *This);
+
+
+typedef struct _EFI_FILE_PROTOCOL {
+    UINT64 Revision;
+    EFI_FILE_OPEN Open;
+    EFI_FILE_CLOSE Close;
+    EFI_FILE_DELETE Delete;
+    EFI_FILE_READ Read;
+    EFI_FILE_WRITE Write;
+    EFI_FILE_GET_POSITION GetPosition;
+    EFI_FILE_SET_POSITION SetPosition;
+    void *GetInfo;
+    void *SetInfo;
+    EFI_FILE_FLUSH Flush;
+    /* Added with revision 2. */
+    void *OpenEx;
+    void *ReadEx;
+    void *WriteEx;
+    void *FlushEx;
+} EFI_FILE_PROTOCOL;
+
+#define EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID \
+ {0x964e5b22,0x6459,0x11d2,{0x8e,0x39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
+#define EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_REVISION 0x00010000
+
+struct _EFI_SIMPLE_FILE_SYSTEM_PROTOCOL;
+
+typedef EFI_STATUS (EFIAPI *EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_OPEN_VOLUME) (IN struct _EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *This,
+                                                                          OUT EFI_FILE_PROTOCOL **Root);
+
+typedef struct _EFI_SIMPLE_FILE_SYSTEM_PROTOCOL {
+    UINT64 Revision;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_OPEN_VOLUME OpenVolume;
+} EFI_SIMPLE_FILE_SYSTEM_PROTOCOL;
--- pdpclib/efistart.c
+++ pdpclib/efistart.c
@@ -10,7 +10,14 @@
  *****************************************************************************/
 #include "efi.h"
 
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
 EFI_SYSTEM_TABLE *gST;
+EFI_BOOT_SERVICES *gBS;
+
+#define return_Status_if_fail(func) do { if ((Status = (func))) { return Status; }} while (0)
 
 static EFI_STATUS print_string (char *str) {
 
@@ -40,14 +47,75 @@ static EFI_STATUS print_string (char *str) {
 
 }
 
+static EFI_STATUS wait_for_input (void) {
+
+    EFI_STATUS Status;
+    UINTN Index;
+
+    return_Status_if_fail (print_string ("Press any key to continue\n"));
+
+    return_Status_if_fail (gST->BootServices->WaitForEvent (1, &gST->ConIn->WaitForKey, &Index));
+
+    Status = gST->ConIn->Reset (gST->ConIn, 0);
+
+    return Status;
+
+}
+
+static EFI_STATUS obtain_Root (EFI_HANDLE ImageHandle, EFI_FILE_PROTOCOL **Root) {
+
+    EFI_STATUS Status = EFI_SUCCESS;
+    EFI_GUID li_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+    EFI_LOADED_IMAGE_PROTOCOL *li_protocol;
+    EFI_GUID sfs_protocol_guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *sfs_protocol;
+
+    return_Status_if_fail (gBS->HandleProtocol (ImageHandle, &li_guid, (void **)&li_protocol));
+    return_Status_if_fail (gBS->HandleProtocol (li_protocol->DeviceHandle, &sfs_protocol_guid, (void **)&sfs_protocol));
+    return_Status_if_fail (sfs_protocol->OpenVolume (sfs_protocol, Root));
+
+    return Status;
+
+}    
+
+static EFI_STATUS file_test (EFI_HANDLE ImageHandle) {
+    
+    EFI_STATUS Status = EFI_SUCCESS;
+    EFI_FILE_PROTOCOL *Root;
+    EFI_FILE_PROTOCOL *new_file;
+    CHAR16 file_name[] = {'t','e','s','t','.','t','x','t','\0'};
+    UINT64 OpenMode = {0x3, 0x80000000}; /* Read+Write+Create */
+    UINT64 Attributes = {0, 0};
+    char content[] = "This is a test file created by efistart.c.\n";
+    UINTN bytes_to_write = sizeof (content) - 1;
+
+    return_Status_if_fail (obtain_Root (ImageHandle, &Root));
+
+    return_Status_if_fail (print_string ("Opening file\n"));
+
+    return_Status_if_fail (Root->Open (Root, &new_file, file_name, OpenMode, Attributes));
+
+    return_Status_if_fail (print_string ("Writing file\n"));
+
+    return_Status_if_fail (new_file->Write (new_file, &bytes_to_write, content));
+
+    return_Status_if_fail (print_string ("Closing file\n"));
+
+    return_Status_if_fail (new_file->Close (new_file));
+
+    return Status;
+
+}
+
 EFI_STATUS efimain (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
 
-    EFI_STATUS Status;
+    EFI_STATUS Status, Status2;
     UINTN Index;
     UINT64 dummy_watchdog_code = {0xFFFFFFFFLU, 0xFFFFFFFFLU};
 
     gST = SystemTable;
+    gBS = gST->BootServices;
 
     gST->BootServices->SetWatchdogTimer (0, dummy_watchdog_code, 0, (CHAR16 *)0);
 
@@ -55,7 +123,17 @@ EFI_STATUS efimain (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
         return Status;
     }
 
-    if ((Status = print_string("Press any key to exit\n"))) {
+    wait_for_input ();
+    
+    if ((Status2 = file_test (ImageHandle))) {
+        
+        return_Status_if_fail (print_string ("File test FAILED\n"));
+        wait_for_input ();
+        return Status2;
+        
+    }
+
+    if ((Status = print_string("All tests succeeded\nPress any key to exit\n"))) {
         return Status;
     }
     
