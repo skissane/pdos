2d5c97883293021d431a6f278bfe6a68a20f0426
Paul Edwards <mutazilah@gmail.com>
2023-03-09 06:08:02 +0800

    support functions for 64-bit

---
+++ pdpclib/x64supc.c
@@ -0,0 +1,15 @@
+/* written by Paul Edwards */
+/* released to the public domain */
+/* x64supc - support routines for x64 */
+
+void __setj(void)
+{
+    return;
+}
+
+void __longj(void)
+{
+    return;
+}
+
+int _fltused = 0;
