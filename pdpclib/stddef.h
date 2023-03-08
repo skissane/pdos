83352810246db3483691e80896bb52f36ce296a6
Paul Edwards <mutazilah@gmail.com>
2023-03-09 06:07:32 +0800

    support 64-bit size_t

--- pdpclib/stddef.h
+++ pdpclib/stddef.h
@@ -21,6 +21,8 @@ typedef int ptrdiff_t;
     || defined(__ARM__) || defined(__gnu_linux__) || defined(__PDOS386__) \
     || defined(__SZ4__))
 typedef unsigned long size_t;
+#elif defined(__64BIT__) && defined(__EFI__)
+typedef unsigned long long size_t;
 #elif (defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC) \
     || defined(__WIN32__) || defined(__AMIGA__) || defined(__EFI__))
 typedef unsigned int size_t;
--- pdpclib/stdlib.h
+++ pdpclib/stdlib.h
@@ -20,6 +20,8 @@
     || defined(__ARM__)|| defined(__gnu_linux__) || defined(__PDOS386__) \
     || defined(__SZ4__))
 typedef unsigned long size_t;
+#elif defined(__64BIT__) && defined(__EFI__)
+typedef unsigned long long size_t;
 #elif (defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC) \
     || defined(__WIN32__) || defined(__AMIGA__) || defined(__EFI__))
 typedef unsigned int size_t;
