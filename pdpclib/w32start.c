8702f0300856f9307238f500e8c815b89699ce59
Paul Edwards <mutazilah@gmail.com>
2023-03-27 12:18:47 +0800

    add some documentation

--- pdpclib/w32start.c
+++ pdpclib/w32start.c
@@ -20,6 +20,17 @@ int main(int argc, char **argv);
 
 /* This is the main entry point of a console mode executable */
 
+/* My understanding is that prior to this code being executed, the
+   dependency on msvcrt.dll will have caused it to be loaded first.
+   And it was built with PDPCLIB_DLL and included dllcrt.c, and the
+   entry point DllMainCRTStartup will be called, which does a call
+   to __start, and that call (part of the DLL) exits early - before
+   calling main(). So then, when the main executable is loaded,
+   mainCRTStartup is then called, and the only thing that hasn't
+   been done (for whatever reason, presumably design choice) is
+   parsing the parameters. The DLL has a function (getmainargs)
+   to do that too, so that is now called */
+
 /* consider adding this line so that relocatables are generated
    for some/most/all versions of "ld". The reason for this is
    that, at least with binutils 2.14a, there is code in there
