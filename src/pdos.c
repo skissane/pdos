734d5a0d11bf12a10aad5b5b95406102f59ad174
Paul Edwards <mutazilah@gmail.com>
2023-04-21 18:06:26 +0800

    keyboard remap now working

--- src/pdos.c
+++ src/pdos.c
@@ -288,7 +288,8 @@ static int stderr_fhandle_index = 2;
 
 static int showret = 0;
 
-static int keybmap_active = 1;
+static int keybmap_active = 0;
+/* note that all these one to one definitions are not currently used */
 static unsigned char keybmap[256] =
     "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
     "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
