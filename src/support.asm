dd571d57aba668e581e808448a50d7c54b8785ab
Paul Edwards <mutazilah@gmail.com>
2023-04-24 10:52:13 +0800

    support bp as an RM16 interrupt register

--- src/support.asm
+++ src/support.asm
@@ -291,6 +291,19 @@ popf
 mov ax, word ptr [si + 0]
 mov bx, word ptr [si + 2]
 mov cx, word ptr [si + 4]
+
+; now get bp ready
+; but first, there is still a ds on the stack. we need
+; to swap the order. we'll use dx as a scratch register
+pop dx
+; first preserve bp, for restoration after interrupt
+push bp
+; we'll use di instead of bp as a scratch register
+mov di, word ptr [si + 16]
+; now ready to be loaded
+push di  ; really bp from caller
+push dx ; really ds
+
 mov dx, word ptr [si + 6]
 mov di, word ptr [si + 10]
 mov si, word ptr [si + 8]
@@ -299,66 +312,77 @@ pop ds; load previously saved value for ds
 
 cmp intnum, 08h
 jne xnot8
+pop bp
 int 08h
 jmp xfintry
 xnot8:
 
 cmp intnum, 09h
 jne xnot9
+pop bp
 int 09h
 jmp xfintry
 xnot9:
 
 cmp intnum, 010h
 jne xnot10
+pop bp
 int 010h
 jmp xfintry
 xnot10:
 
 cmp intnum, 013h
 jne xnot13
+pop bp
 int 013h
 jmp xfintry
 xnot13:
 
 cmp intnum, 014h
 jne xnot14
+pop bp
 int 014h
 jmp xfintry
 xnot14:
 
 cmp intnum, 015h
 jne xnot15
+pop bp
 int 015h
 jmp xfintry
 xnot15:
 
 cmp intnum, 016h
 jne xnot16
+pop bp
 int 016h
 jmp xfintry
 xnot16:
 
 cmp intnum, 01Ah
 jne xnot1A
+pop bp
 int 01Ah
 jmp xfintry
 xnot1A:
 
 cmp intnum, 021h
 jne xnot21
+pop bp
 int 021h
 jmp xfintry
 xnot21:
 
 cmp intnum, 025h
 jne xnot25
+pop bp
 int 025h
 jmp xfintry
 xnot25:
 
 cmp intnum, 026h
 jne xnot26
+pop bp
 int 026h
 jmp xfintry
 xnot26:
@@ -366,36 +390,42 @@ xnot26:
 ; Copied BIOS interrupts for PDOS-32.
 cmp intnum, 0A0h
 jne xnotA0
+pop bp
 int 0A0h
 jmp xfintry
 xnotA0:
 
 cmp intnum, 0A3h
 jne xnotA3
+pop bp
 int 0A3h
 jmp xfintry
 xnotA3:
 
 cmp intnum, 0A4h
 jne xnotA4
+pop bp
 int 0A4h
 jmp xfintry
 xnotA4:
 
 cmp intnum, 0A5h
 jne xnotA5
+pop bp
 int 0A5h
 jmp xfintry
 xnotA5:
 
 cmp intnum, 0A6h
 jne xnotA6
+pop bp
 int 0A6h
 jmp xfintry
 xnotA6:
 
 cmp intnum, 0AAh
 jne xnotAA
+pop bp
 int 0AAh
 jmp xfintry
 xnotAA:
@@ -403,28 +433,36 @@ xnotAA:
 ; Copied BIOS IRQ handler interrupts for PDOS-32.
 cmp intnum, 0B0h
 jne xnotB0
+pop bp
 int 0B0h
 jmp xfintry
 xnotB0:
 
 cmp intnum, 0B1h
 jne xnotB1
+pop bp
 int 0B1h
 jmp xfintry
 xnotB1:
+
+; if we have no interrupts left to try, we still need to clean up the stack
+pop bp
 
 xfintry:
 
 push es
 push ds
+push bp ; new value
 push si
 push ax
-push bp
 
 mov bp, sp
-mov ax, [bp+10]; restore ds immediately, can't move without it
-pop bp
+mov ax, [bp+12]; restore ds immediately, can't move without it
 mov ds, ax
+
+mov ax, [bp+10] ; this is the old, not new (from interrupt), bp
+push ax
+pop bp
 
 mov ax, 0
 jnc xflagclear
@@ -463,6 +501,8 @@ mov [si + 6], dx
 mov [si + 10], di
 pop ax
 mov [si + 8], ax ; si
+pop ax
+mov [si + 16], ax ; bp
 
 ifndef SMALLERC
 
@@ -483,6 +523,8 @@ pop ax
 mov [si + 6], ax; restore ds
 pop ax
 mov [si + 0], ax ; restore es
+
+pop bp  ; this has already been set, but we need to clean up the stack
 
 pop ds  ; restore value saved over interrupt (but accessed directly already)
 
