fdc9663a227517569db7eb0e2c3ba1060214dbeb
Paul Edwards <mutazilah@gmail.com>
2023-04-27 16:21:27 +0800

    use pdld instead of ldwin

--- src/compk32.bat
+++ src/compk32.bat
@@ -24,6 +24,6 @@ gccwin -S -O2 -ansi -DNOLIBALLOC -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . ../p
 aswin -o pdossupc.o pdossupc.s
 del pdossupc.s
 
-ldwin -s -o kernel32.dll --shared --kill-at dllcrt.o kernel32.o pos.o support.o string.o pdossupc.o
+pdld -s -o kernel32.dll --shared --kill-at dllcrt.o kernel32.o pos.o support.o string.o pdossupc.o
 
 dlltwin -S aswin -k --export-all-symbols -D kernel32.dll kernel32.o -l kernel32.a
