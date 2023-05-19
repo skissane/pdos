191b90c3624845c22e5dcbd8e2d9a34165889ee8
Paul Edwards <mutazilah@gmail.com>
2023-05-20 07:40:31 +0800

    switch to using a makefile

--- src/comp6w.bat
+++ src/comp6w.bat
@@ -1,9 +1,1 @@
-gcc386 -S -O2 -ansi -fno-common -D__PDOS386__ -D__32BIT__ -I../pdpclib pcomm.c
-gcc386 -S -O2 -ansi -fno-common -D__PDOS386__ -D__32BIT__ -I../pdpclib dostime.c
-
-as386 -o pcomm.o pcomm.s
-del pcomm.s
-as386 -o dostime.o dostime.s
-del dostime.s
-
-ld86 -N -s -e ___pdosst32 -o pcomm.exe ../pdpclib/pdosst32.o pcomm.o dostime.o ../pdpclib/pdos.a
+pdmake -f makecomm.std
