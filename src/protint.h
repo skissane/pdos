b6acc451e97c4655ba541087c9db3c6eca6fe53e
Paul Edwards <mutazilah@gmail.com>
2023-05-23 11:05:54 +0800

    stop using watcom reserved word - interrupt

--- src/protint.h
+++ src/protint.h
@@ -75,7 +75,7 @@ extern rawprot_parms *rp_parms;
 unsigned long rawprot_p(rawprot_parms *parmlist);
 unsigned long runprot_p(rawprot_parms *parmlist);
 unsigned long runaout_p(rawprot_parms *parmlist);
-void protintHandler(int interrupt, int (*func)(unsigned int *));
-void *protintGetHandler(int interrupt);
+void protintHandler(int interrpt, int (*func)(unsigned int *));
+void *protintGetHandler(int interrpt);
 
 #endif
