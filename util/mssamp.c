e9c8d671304ada32d33bdfe0ba5065f5a245e9bb
Paul Edwards <mutazilah@gmail.com>
2023-05-28 07:51:58 +0800

    sample program for multisc.c

---
+++ util/mssamp.c
@@ -0,0 +1,59 @@
+/* ***************************************************************** */
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/* ***************************************************************** */
+/* ***************************************************************** */
+/*                                                                   */
+/*  mssamp - sample program that can be handled by multisc           */
+/*                                                                   */
+/*  This program just prints ABCDE                                   */
+/*                                                                   */
+/*  compile like this:                                               */
+/*  multisc mssamp.c mssamp.com                                      */
+/*                                                                   */
+/* ***************************************************************** */
+
+int ret; /* must be this name */
+int x;
+
+/* this will probably be in a separate file, and pdcc used to
+   merge them together this first. It needs to be seen before
+   "main" calls it */
+
+void display()
+{
+    /* mov ah, 2    B4 02 */
+    asm 180; asm 2;
+    /* mov dl, ds:[0xxyy]    8A 16 yy xx */
+    /* 383ch is "ret" variable */
+    asm 138; asm 22; asm 60; asm 56;
+    /* int 21h    CD 21 */
+    asm 205; asm 33;
+}
+
+/* this is the only function you really need to read */
+
+void main()
+{
+    x = 60;
+    ret = x + 5;
+    while( ret < 70 ){
+        display();
+        ret = ret + 1;
+    }
+    ret = 13;
+    display();
+    ret = 10;
+    display();
+    ret = 0; /* return code to operating system */
+}
+
+/* this probably belongs in a separate file, for pdcc to
+   merge last */
+
+void _start()
+{
+    main();
+}
