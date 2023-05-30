f8979b4c5b4e715a374dad06f8266338bdafd1e0
Paul Edwards <mutazilah@gmail.com>
2023-05-31 01:22:25 +0800

    pdos disk no longer compressed

--- s370/doit.bat
+++ s370/doit.bat
@@ -114,7 +114,7 @@ del pdospkg.zip
 zip -9X pdospkg pload.sys pdos.sys config.sys readme.txt
 zip -9X pdospkg pcomm.exe autoexec.bat world.exe bbs.exe sample.c
 zip -9X pdospkg wtoworld.exe diff.exe uemacs.exe edlin.exe hercauto.zip
-zip -9X pdospkg ctl.txt pdos00.cckd pdos.cnf auto*.rc termherc*.rc
+zip -9X pdospkg ctl.txt pdos00.ckd pdos.cnf auto*.rc termherc*.rc
 zip -9X pdospkg runpdos.bat pdos.bat pdpi.zip pdpnntp.exe mkipltap.exe
 zip -9X pdospkg gcc.exe mvsendec.exe mvsunzip.exe hexdump.exe copyfile.exe
 zip -9X pdospkg pdmake.exe antitwit.txt pdos.img mkiplcrd.exe wtowcom.com
