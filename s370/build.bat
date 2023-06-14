a4f68c24293b63cab25ef4c9302ea4bd5120bbc9
Paul Edwards <mutazilah@gmail.com>
2023-06-14 16:56:47 +0800

    bug fixes

--- s370/build.bat
+++ s370/build.bat
@@ -20,16 +20,16 @@ echo after hercules/380 starts, run startterm then ipl 1b9
 echo then mkipltap tav1c2:
 echo then quit
 hercules -f pdos.cnf
-copy out.aws dist\tapes\ipltap.aws
+copy tapes\out.aws dist\tapes\ipltap.aws
 
 echo after hercules starts the usual way do: dsk2fil 1b9 tap1c2:
 hercules -f pdos.cnf
-copy out.aws dist\tapes\disk3390.aws
+copy tapes\out.aws dist\tapes\disk3390.aws
 
 echo after hercules start, do: mkiplmem dev1c2:
 hercules -f pdos.cnf
-hetget -b tapes\out.aws dist\pdosmem.img 1
+hetget -b -n tapes\out.aws dist\pdosmem.img 1 U 0 32760
 
 echo after hercules start, do: mkiplcrd dev1c2:
 hercules -f pdos.cnf
-hetget -b tapes\out.aws dist\crd3270.dat 1
+hetget -b -n tapes\out.aws dist\tapes\iplcrd.dat 1 U 0 32760
