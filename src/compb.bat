fd8917b285ae0f9e6deae10b3e93f25dde407459
Paul Edwards <mutazilah@gmail.com>
2023-07-20 23:20:03 +0800

    allow assembly with masm 6.11

--- src/compb.bat
+++ src/compb.bat
@@ -9,3 +9,6 @@ rem wlink File pbootsec.obj Name pbootsec.com Form dos com Option quiet
 
 as86 -Dmemodel=tiny -o pbootsec.o pbootsec.asm
 ld86 -o pbootsec.com --oformat msdos pbootsec.o
+
+rem ml -c -Dmemodel=tiny pbootsec.asm
+rem wlink File pbootsec.obj Name pbootsec.com Form dos com Option quiet
