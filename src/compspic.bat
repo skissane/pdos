97b3905bc7a4bc2a452630b20cd82be9db46c92c
Paul Edwards <mutazilah@gmail.com>
2023-07-31 21:37:10 +0800

    compile showpic for PDOS/86

--- src/compspic.bat
+++ src/compspic.bat
@@ -1,3 +1,9 @@
+rem wasm -zq -zcm -Dmemodel=large support.asm
+rem wcl -ecc -zp1 -q -w -c -I. -ml -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib showpic.c bos.c
+rem wlink File showpic.obj,bos.obj,support.obj Name showpic.exe Form dos Library ..\pdpclib\watcom.lib Option quiet,stack=8192,start=___asmstart,dosseg
+
+
+
 gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__WIN32__ showpic.c
 aswin -o showpic.o showpic.s
 del showpic.s
