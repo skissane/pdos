7d8bcc3a741058be19168daffa93318bf123b64d
Paul Edwards <mutazilah@gmail.com>
2023-08-16 22:11:34 +0800

    stubs to allow 32-bit code to execute on a 64-bit environment

---
+++ generic/w32hack.asm
@@ -0,0 +1,41 @@
+# Written by Paul Edwards
+# Released to the public domain
+
+# Glue functions for supporting Win32 executables in
+# a 64-bit environment
+
+.code64
+.intel_syntax noprefix
+
+# 
+.align 8
+.globl w32puts
+w32puts:
+
+push rbp
+mov rbp,rsp
+
+xor rcx, rcx
+# 8 bytes for rbp followed by 4 bytes for return address
+# gives the 4 bytes for parameter
+mov ecx, 12[rbp]
+
+# Ensure stack is 16-byte aligned
+push rax
+call puts
+pop rax
+
+# return code 0
+xor rax,rax
+
+# Load return address
+xor rcx, rcx
+mov ecx, 8[rbp]
+pop rbp
+
+# Clear return address
+add rsp, 8
+
+jmp rcx
+
+ret
