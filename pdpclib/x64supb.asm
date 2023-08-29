0c7fd79cd6a242cf29340f1abc3fe483d2f70819
Paul Edwards <mutazilah@gmail.com>
2023-08-29 09:13:25 +0800

    preserve more registers in setjmp/longjmp

--- pdpclib/x64supb.asm
+++ pdpclib/x64supb.asm
@@ -10,6 +10,11 @@
 # plus return address
 # And first location is reserved for return value
 # (second is for return address)
+# After getting an issue, added rsi and rdi to preserve
+# That didn't fix the problem, so I added r10-r15
+# That didn't fix the problem, so I added rbp
+# That fixed the problem, and I just left everything in,
+# as that is probably the best thing to do anyway
 .globl __setj
 __setj:
 mov [rcx + 8*2], rbx
@@ -18,6 +23,15 @@ mov [rcx + 8*4], rdx
 mov [rcx + 8*5], r8
 mov [rcx + 8*6], r9
 mov [rcx + 8*7], rsp
+mov [rcx + 8*8], rsi
+mov [rcx + 8*9], rdi
+mov [rcx + 8*10], r10
+mov [rcx + 8*11], r11
+mov [rcx + 8*12], r12
+mov [rcx + 8*13], r13
+mov [rcx + 8*14], r14
+mov [rcx + 8*15], r15
+mov [rcx + 8*16], rbp
 mov rax, [rsp]
 mov [rcx + 8*1], rax
 # we only return int (eax), but since we're not preserving
@@ -36,6 +50,15 @@ mov rbx, [rcx + 8*2]
 mov rdx, [rcx + 8*4]
 mov r8, [rcx + 8*5]
 mov r9, [rcx + 8*6]
+mov rsi, [rcx + 8*8]
+mov rdi, [rcx + 8*9]
+mov r10, [rcx + 8*10]
+mov r11, [rcx + 8*11]
+mov r12, [rcx + 8*12]
+mov r13, [rcx + 8*13]
+mov r14, [rcx + 8*14]
+mov r15, [rcx + 8*15]
+mov rbp, [rcx + 8*16]
 mov rax, [rcx]
 mov rcx, [rcx + 8*3]
 ret
