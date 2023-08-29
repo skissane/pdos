8674f300756fe595064a3ed69fe230aee3824657
Paul Edwards <mutazilah@gmail.com>
2023-08-29 15:44:14 +0800

    more dummy functions

--- cc64/junk.c
+++ cc64/junk.c
@@ -9,3 +9,24 @@ int m$popcallback(void) { return (0); }
 int m$pushcallback(void) { return (0); }
 int RegisterClassExA(void) { return (0); }
 int LoadCursorA(void) { return (0); }
+
+int GetTickCount(void) { return (0); }
+int QueryPerformanceFrequency(void) { return (0); }
+int QueryPerformanceCounter(void) { return (0); }
+int GetLocalTime(void) { return (0); }
+int CreateFileA(void) { return (0); }
+int GetModuleFileNameA(void) { return (0); }
+int Sleep(void) { return (0); }
+int ReadConsoleInputA(void) { return (0); }
+int GetProcAddress(void) { return (0); }
+int LoadLibraryA(void) { return (0); }
+int FlushConsoleInputBuffer(void) { return (0); }
+int GetNumberOfConsoleInputEvents(void) { return (0); }
+int CloseHandle(void) { return (0); }
+int GetExitCodeProcess(void) { return (0); }
+int WaitForSingleObject(void) { return (0); }
+int GetLastError(void) { return (0); }
+int CreateProcessA(void) { return (0); }
+int SetConsoleMode(void) { return (0); }
+int SetConsoleCtrlHandler(void) { return (0); }
+int GetStdHandle(void) { return (0); }
