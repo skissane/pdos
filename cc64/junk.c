94b4edf5aae5af1d14d261e4e0d0c8dd6bad3397
Paul Edwards <mutazilah@gmail.com>
2023-09-03 20:33:56 +0800

    remove dummy pop and push

--- cc64/junk.c
+++ cc64/junk.c
@@ -5,8 +5,6 @@ int PeekMessageA(void) { return (0); }
 int MessageBoxA(void) { return (0); }
 int GetFileTime(void) { return (0); }
 int DefWindowProcA(void) { return (0); }
-int m$popcallback(void) { return (0); }
-int m$pushcallback(void) { return (0); }
 int RegisterClassExA(void) { return (0); }
 int LoadCursorA(void) { return (0); }
 
