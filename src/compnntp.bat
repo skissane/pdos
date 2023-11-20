897a8f69cae5d40ba9987df5653f4f8ee3569276
Paul Edwards <mutazilah@gmail.com>
2023-11-20 17:43:34 +0800

    use new standard for compilation

--- src/compnntp.bat
+++ src/compnntp.bat
@@ -1,5 +1,5 @@
 gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__WIN32__ pdpnntp.c
-aswin -o pdpnntp.o pdpnntp.s
+pdas -o pdpnntp.obj --oformat coff pdpnntp.s
 del pdpnntp.s
 
-ldwin -o pdpnntp.exe ../pdpclib/w32start.o pdpnntp.o ../pdpclib/msvcrt.a
+pdld -s -nostdlib --no-insert-timestamp -o pdpnntp.exe ../pdpclib/w32start.obj pdpnntp.obj ../pdpclib/msvcrt.lib
