40388301af5ebad4dda84e97261f403b01c9fa76
Paul Edwards <mutazilah@gmail.com>
2023-12-01 20:34:09 +0800

    support SubC

--- src/pdos.h
+++ src/pdos.h
@@ -4,8 +4,23 @@
 
 typedef struct {
     unsigned long transferbuf;
+#ifdef __SUBC__
+    int dummy1;
+#endif
     unsigned long doreboot;
+#ifdef __SUBC__
+    int dummy2;
+#endif
     unsigned long bpb;
+#ifdef __SUBC__
+    int dummy3;
+#endif
     unsigned long dopoweroff;
+#ifdef __SUBC__
+    int dummy4;
+#endif
     unsigned long doboot;
+#ifdef __SUBC__
+    int dummy5;
+#endif
 } pdos_parms;
