0d747bc0a0f59a9888fbec373c7a1a6cfbf26f3c
Paul Edwards <mutazilah@gmail.com>
2023-12-15 04:44:32 +0800

    stop using MSC6 now

--- src/compbf32.bat
+++ src/compbf32.bat
@@ -5,8 +5,8 @@ rem tlink -t -x fat32,fat32.com,,,
 rem wasm -zq -zcm -Dmemodel=tiny fat32.asm
 rem wlink File fat32.obj Name fat32.com Form dos com Option quiet
 
-as86 -DMSC6 -Dmemodel=tiny -o fat32.o fat32.asm
-rem as86 -Dmemodel=tiny -o fat32.o fat32.asm
+rem as86 -DMSC6 -Dmemodel=tiny -o fat32.o fat32.asm
+as86 -Dmemodel=tiny -o fat32.o fat32.asm
 ld86 -o fat32.com --oformat msdos fat32.o
 
 rem ml -c -Dmemodel=tiny fat32.asm
