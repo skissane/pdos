9566b0a6a6d0395c030bedff0828abc9f6324a13
Paul Edwards <mutazilah@gmail.com>
2023-12-15 18:16:20 +0800

    Shift and Incr don't return anything as far as I am aware

--- pdpclib/fpfuncsw.c
+++ pdpclib/fpfuncsw.c
@@ -52,8 +52,8 @@ int __cdmain(int argc, char **argv)
 #ifdef __OS2__
 #include <os2.h>
 
-void * APIENTRY DosHugeShift(void);
-void * APIENTRY DosHugeIncr(void);
+void APIENTRY DosHugeShift(void);
+void APIENTRY DosHugeIncr(void);
 
 void __myDosExit(int one, int two)
 {
