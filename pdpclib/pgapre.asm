8bf58b9733dcc98b4f4f19a2863052b9e5249ac0
Paul Edwards <mutazilah@gmail.com>
2023-12-16 10:46:25 +0800

    prefix code required by 16-bit PDOS-generic apps

---
+++ pdpclib/pgapre.asm
@@ -0,0 +1,43 @@
+; pgapre.asm - prefix code for PDOS-generic 16-bit programs
+;
+; This program written by Paul Edwards
+; Released to the public domain
+
+% .model memodel, c
+
+extrn __crt0: proc
+
+.stack 4000h
+
+.data
+
+; eyecatcher and padding
+public __eyepad
+__eyepad db 'XXXXXXXXXXXXXXXX'
+
+.code
+
+top:
+
+public __intstart
+__intstart proc
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+jmp __crt0
+__intstart endp
+
+end top
