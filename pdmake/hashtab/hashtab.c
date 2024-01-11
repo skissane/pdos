158e95acc51a27ae88fbba02a41521871bccdc03
Paul Edwards <mutazilah@gmail.com>
2024-01-12 06:29:56 +0800

    stop using floating point for pdos-generic ARM executables

--- pdmake/hashtab/hashtab.c
+++ pdmake/hashtab/hashtab.c
@@ -96,7 +96,14 @@ static int rehash (struct hashtab *hashtab, size_t target_size)
     }
     
     hashtab->probe_limit = max (internal_log2 (hashtab->size), MINIMAL_PROBE_LIMIT);
+#ifdef __ARMGEN__
+    /* ARM has problem with floating point,
+     * so assume max_load_factor is always 0.5 and just divide by 2, rounding up.
+     */
+    hashtab->max_number_of_elements = hashtab->size / 2 + hashtab->size % 2;
+#else
     hashtab->max_number_of_elements = ceil (hashtab->max_load_factor * hashtab->size);
+#endif
     
     /* Allocates size + probe_limit + 1 so no bounds checking needs to be done. */
     hashtab->entries = hashtab->malloc_func (sizeof (hashtab->entries[0]) * (hashtab->size + hashtab->probe_limit + 1));
