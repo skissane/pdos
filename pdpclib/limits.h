65dba3b00fda96866ca93aa29fc1b1ce20682ca2
Paul Edwards <mutazilah@gmail.com>
2024-01-20 17:54:44 +0800

    proper limits for 64-bit

--- pdpclib/limits.h
+++ pdpclib/limits.h
@@ -26,8 +26,13 @@
 #endif
 #define MB_LEN_MAX 1
 #define SHRT_MIN (-SHRT_MAX-1)
+#ifdef __FORCE_ALL_64__
+#define SHRT_MAX 0x7fffffffffffffffL
+#define USHRT_MAX ((unsigned short)0xffffffffffffffffUL)
+#else
 #define SHRT_MAX 32767
 #define USHRT_MAX ((unsigned short)65535U)
+#endif
 
 #if (defined(__OS2__) || defined(__32BIT__) || defined(__MVS__) \
     || defined(__VSE__) || defined(__PDOS386__) \
@@ -35,8 +40,15 @@
     || defined(__AMIGA__) ||defined(__SMALLERC__) || defined(__ARM__) \
     || defined(__EFI__))
 #define INT_MIN (-INT_MAX-1)
+
+#ifdef __FORCE_ALL_64__
+#define INT_MAX 0x7fffffffffffffffL
+#define UINT_MAX ((unsigned short)0xffffffffffffffffUL)
+#else
 #define INT_MAX 2147483647
 #define UINT_MAX 4294967295U
+#endif
+
 #endif
 
 #if (defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC))
@@ -46,7 +58,13 @@
 #endif
 
 #define LONG_MIN (-LONG_MAX-1)
+
+#if defined(__64BIT__) && defined(__gnu_linux__)
+#define LONG_MAX 0x7fffffffffffffffL
+#define ULONG_MAX ((unsigned short)0xffffffffffffffffUL)
+#else
 #define LONG_MAX 2147483647L
 #define ULONG_MAX 4294967295UL
+#endif
 
 #endif
