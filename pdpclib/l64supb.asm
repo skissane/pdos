0e7e2208e0c2b71fac720a37a8c87e7687a62f6b
Paul Edwards <mutazilah@gmail.com>
2024-01-27 22:27:32 +0800

    comment out STACKPARM for now

--- pdpclib/l64supb.asm
+++ pdpclib/l64supb.asm
@@ -112,34 +112,34 @@ ___read:
 .globl __read
 __read:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
 push %rdx
-.endif
+.endif */
 
 # function code 0 = read
 movq $0, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # handle
 movq 16(%rbp), %rdi
 # data pointer
 movq 24(%rbp), %rsi
 # length
 movq 32(%rbp), %rdx
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdx
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -151,34 +151,34 @@ ___open:
 .globl __open
 __open:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
 push %rdx
-.endif
+.endif */
 
 # function code 2 = open
 movq $2, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # filename
 movq 16(%rbp), %rdi
 # flag
 movq 24(%rbp), %rsi
 # mode
 movq 32(%rbp), %rdx
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdx
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -189,34 +189,34 @@ __seek:
 .globl ___seek
 ___seek:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
 push %rdx
-.endif
+.endif */
 
 # function code 8 = lseek
 movq $8, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # handle
 movq 16(%rbp), %rdi
 # offset
 movq 24(%rbp), %rsi
 # whence
 movq 32(%rbp), %rdx
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdx
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -227,30 +227,30 @@ ___rename:
 .globl __rename
 __rename:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
-.endif
+.endif */
 
 # function code 82 = rename
 movq $82, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # old file
 movq 16(%rbp), %rdi
 # new file
 movq 24(%rbp), %rsi
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -260,26 +260,26 @@ ___remove:
 .globl __remove
 __remove:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
-.endif
+.endif */
 
 # function code 87 = unlink
 movq $87, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # filename
 movq 16(%rbp), %rdi
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -290,26 +290,26 @@ ___close:
 .globl __close
 __close:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
-.endif
+.endif */
 
 # function code 3 = close
 movq $3, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # handle
 movq 16(%rbp), %rdi
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -322,21 +322,21 @@ ___exita:
 __exita:
 # exit/terminate
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 movq 16(%rbp), %rdi
-.endif
+.endif */
 
 movq $60, %rax
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -346,26 +346,26 @@ ___time:
 .globl __time
 __time:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
-.endif
+.endif */
 
 # function code 201 = retrieve current time
 movq $201, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # pointer to time_t
 movq 16(%rbp), %rdi
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -377,34 +377,34 @@ ___ioctl:
 .globl __ioctl
 __ioctl:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
 push %rdx
-.endif
+.endif */
 
 # function code 16 = ioctl
 movq $16, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # file descriptor
 movq 16(%rbp), %rdi
 # command
 movq 24(%rbp), %rsi
 # parameter
 movq 32(%rbp), %rdx
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdx
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -415,26 +415,26 @@ ___chdir:
 .globl __chdir
 __chdir:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
-.endif
+.endif */
 
 # function code 80 = chdir
 movq $80, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # filename (directory name)
 movq 16(%rbp), %rdi
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -445,26 +445,26 @@ ret
 ___rmdir:
 .globl __rmdir
 __rmdir:
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
-.endif
+.endif */
 
 # function code 84 = rmdir
 movq $84, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # pathname
 movq 8(%rbp), %rdi
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -476,30 +476,30 @@ ___mkdir:
 .globl __mkdir
 __mkdir:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
-.endif
+.endif */
 
 # function code 83 = mkdir
 movq $83, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # pathname
 movq 16(%rbp), %rdi
 # mode
 movq 24(%rbp), %rsi
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -511,34 +511,34 @@ ___getdents:
 .globl __getdents
 __getdents:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
 push %rdx
-.endif
+.endif */
 
 # function code 78 = getdents
 movq $78, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # file descriptor
 movq 16(%rbp), %rdi
 # dirent
 movq 24(%rbp), %rsi
 # count
 movq 32(%rbp), %rdx
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdx
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
 
@@ -550,33 +550,33 @@ ___mprotect:
 .globl __mprotect
 __mprotect:
 
-.if STACKPARM
+/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
 push %rdx
-.endif
+.endif */
 
 # function code 10 = mprotect
 movq $10, %rax
 
-.if STACKPARM
+/* .if STACKPARM
 # start
 movq 16(%rbp), %rdi
 # len
 movq 24(%rbp), %rsi
 # prot
 movq 32(%rbp), %rdx
-.endif
+.endif */
 
 syscall
 
-.if STACKPARM
+/* .if STACKPARM
 pop %rdx
 pop %rsi
 pop %rdi
 pop %rbp
-.endif
+.endif */
 
 ret
