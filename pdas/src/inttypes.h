a96195dd40c6b06d053a2fc26e96cbc6ba755261
Paul Edwards <mutazilah@gmail.com>
2024-01-27 21:08:33 +0800

    fix printf specifier for older non-C99 Microsoft compilers

--- pdas/src/inttypes.h
+++ pdas/src/inttypes.h
@@ -13,24 +13,23 @@
 
 #include <stdint.h>
 
-#ifndef NO_LONG_LONG
-
-#define PRIdFAST64 "lld"
-#define PRIiFAST64 "lli"
-#define PRIoFAST64 "llo"
-#define PRIuFAST64 "llu"
-#define PRIxFAST64 "llx"
-#define PRIXFAST64 "llX"
-
+#if defined(NO_LONG_LONG)
+ #define I64_FMT "l"
+#elif defined(_MSVC_) || defined(__WIN32__)
+ #define I64_FMT "I64"
+#elif defined(__PRI_64_LENGTH_MODIFIER__) /* Mac */
+ #define I64_FMT __PRI_64_LENGTH_MODIFIER__
+#elif defined(SIZEOF_LONG) && SIZEOF_LONG >= 8
+ #define I64_FMT "l"
 #else
-
-#define PRIdFAST64 "ld"
-#define PRIiFAST64 "li"
-#define PRIoFAST64 "lo"
-#define PRIuFAST64 "lu"
-#define PRIxFAST64 "lx"
-#define PRIXFAST64 "lX"
-
+ #define I64_FMT "ll"
 #endif
+
+#define PRIdFAST64 I64_FMT"d"
+#define PRIiFAST64 I64_FMT"i"
+#define PRIoFAST64 I64_FMT"o"
+#define PRIuFAST64 I64_FMT"u"
+#define PRIxFAST64 I64_FMT"x"
+#define PRIXFAST64 I64_FMT"X"
 
 #endif /* __INTTYPES_INCLUDED */
