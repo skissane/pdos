d820e6199803029b0725209dd826c0b2eb391c46
Paul Edwards <mutazilah@gmail.com>
2024-01-28 13:43:17 +0800

    put the entry point here plus other support routines for pdptest to work

--- pdpclib/l64supb.asm
+++ pdpclib/l64supb.asm
@@ -9,6 +9,37 @@
 .code64
 
 
+
+# This is the new entry point. We simply capture
+# the current status of the stack and pass it on
+# to C code (linstart.c, not start.c) figure out
+# the rest. However, it seems that the MSABI
+# requires some stack to be available so that the
+# 4 register parameters can be saved if required.
+# Which seems to be what cc64 does unconditionally.
+# Which means we wipe out the first 4 arguments
+# (argv[x]) that were put on the stack. argc survives
+# presumably because it is in the spot where the
+# return address normally is. Bottom line is we
+# need to subtract 48 bytes from the stack before
+# things get clobbered.
+
+
+
+        .globl ___pdpstart
+___pdpstart:
+mov %rsp, %rcx
+sub $48, %rsp
+call _start
+
+# we shouldn't get here, but if we do, loop
+# instead of doing something random
+loop1:  jmp loop1
+
+
+
+
+
 .globl ___setj
 ___setj:
 .globl __setj
@@ -371,19 +402,18 @@ ret
 
 
 
+# int ___ioctl(unsigned int fd, unsigned int cmd, unsigned long arg);
 
 .globl ___ioctl
 ___ioctl:
 .globl __ioctl
 __ioctl:
 
-/* .if STACKPARM
 push %rbp
 mov %rsp, %rbp
 push %rdi
 push %rsi
 push %rdx
-.endif */
 
 # function code 16 = ioctl
 movq $16, %rax
@@ -397,14 +427,20 @@ movq 24(%rbp), %rsi
 movq 32(%rbp), %rdx
 .endif */
 
+# handle
+xor %rdi,%rdi
+mov %ecx, %edi
+# cmd
+mov %rdx, %rsi
+# arg
+mov %r8, %rdx
+
 syscall
 
-/* .if STACKPARM
 pop %rdx
 pop %rsi
 pop %rdi
 pop %rbp
-.endif */
 
 ret
 
@@ -580,3 +616,115 @@ pop %rbp
 .endif */
 
 ret
+
+
+
+.intel_syntax noprefix
+
+# These routines were copied (and then modified) from bcclib.asm generated
+# by the public domain cc64, and are used for cc64
+# (Converted to PDAS .intel_syntax noprefix by guessing.)
+
+.globl m$ufloat_r64u32
+m$ufloat_r64u32:
+	mov ecx, ecx					# clear top half (already done if value just moved there)
+	cvtsi2sd xmm15, rcx
+	ret
+
+.globl m$ufloat_r32u32
+m$ufloat_r32u32:
+	mov ecx, ecx
+	cvtsi2ss xmm15, rcx
+	ret
+
+.globl m$ufloat_r64u64
+m$ufloat_r64u64:
+#jjj: jmp jjj
+#mov rax, 0x1234567
+#mov rax, 1234567
+#xorpd xmm15, xmm15
+#ret
+
+	cmp ecx, 0
+	jl fl1
+#number is positive, so can treat like i64
+	cvtsi2sd xmm15, rcx
+	ret
+fl1:						#negative value
+	and rcx, QWORD PTR mask63[rip]		#clear top bit (subtract 2**63)
+	cvtsi2sd xmm15, rcx
+	addsd xmm15, QWORD PTR offset64[rip]	#(add 2**63 back to result)
+	ret
+
+.globl m$ufloat_r32u64
+m$ufloat_r32u64:
+	cmp ecx, 0
+	jl fl2
+#number is positive, so can treat like i64
+	cvtsi2ss xmm15, rcx
+	ret
+fl2:						#negative value
+	and rcx, QWORD PTR mask63[rip]		#clear top bit (subtract 2**63)
+	cvtsi2ss xmm15, rcx
+	addss xmm15, DWORD PTR offset32[rip]	#(add 2**63 back to result)
+	ret
+
+.globl m$pushcallback
+m$pushcallback:
+	incd __ncallbacks[rip]
+        push r12
+	mov r12,__ncallbacks[rip]
+	shl r12,6					#8x8 bytes is size per entry
+	lea r11,callbackstack[rip]
+	add r11,r12
+        pop r12
+
+	mov [r11],rbx
+	mov [r11+8],rsi
+	mov [r11+16],rdi
+	mov [r11+24],r12
+	mov [r11+32],r13
+	mov [r11+40],r14
+	mov [r11+48],r15
+	ret
+
+.globl m$popcallback
+m$popcallback:
+        mov r12,__ncallbacks[rip]
+	shl r12,6					#8x8 bytes is size per entry
+	lea r11,callbackstack[rip]
+	add r11,r12
+	mov rbx,[r11]
+	mov rsi,[r11+8]
+	mov rdi,[r11+16]
+	mov r12,[r11+24]
+	mov r13,[r11+32]
+	mov r14,[r11+40]
+	mov r15,[r11+48]
+	decd __ncallbacks[rip]
+	ret
+
+.data
+
+.globl _fltused
+.p2align 2
+_fltused:
+.space 4
+
+.section rdata
+mask63:
+	.quad 0x7fffffffffffffff
+offset64:
+	.quad 9223372036854775808		# 2**63 as r64
+offset32:
+	.quad 9223372036854775808		# 2**63 as r32
+
+
+.bss
+callbackstack:
+	.space 576			#8-level stack
+#	resb 5'120'000
+
+.globl __ncallbacks
+__ncallbacks:
+	.space 8
