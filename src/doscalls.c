201ee7f3bebbaa1c082c41fd0a1436de9a2db3c3
Paul Edwards <mutazilah@gmail.com>
2024-02-26 08:30:34 +0800

    more dummy functions

--- src/doscalls.c
+++ src/doscalls.c
@@ -21,10 +21,89 @@ ULONG APIENTRY DosWrite(ULONG hfile, void *ptr,
                          ULONG towrite, ULONG *tempWritten)
 {
     fwrite(ptr, towrite, 1, stdout);
-    return (1);
+    return (0);
 }
 
 void APIENTRY DosExit(int a, int b)
 {
     exit(b);
+}
+
+ULONG APIENTRY DosOpen(char *fnm, ULONG *handle, ULONG *action1,
+               ULONG newsize, ULONG fileattr, ULONG action2,
+               ULONG mode, ULONG resvd)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosClose(short handle)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosRead(ULONG hfile, void *ptr,
+                       ULONG toread, ULONG *tempRead)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosDelete(char *name)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosMove(char *a, char *b)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosExecPgm(char *err_obj, USHORT sz, USHORT flags,
+                          char *string, void *junk1, RESULTCODES *results,
+                          char *string2)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosSetFilePtr(ULONG hfile, LONG newpos,
+                              int dir, ULONG *retpos)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosGetDateTime(DATETIME *dt)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosDevIOCtl(ULONG handle,
+                           ULONG category,
+                           ULONG function,
+                           void *parmptr,
+                           ULONG parmmax,
+                           ULONG *parmlen,
+                           void *dataptr,
+                           ULONG datamax,
+                           ULONG *datalen)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosAllocMem(void *base, ULONG size, ULONG flags)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosFreeMem(void *base)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosScanEnv(void *name, void *result)
+{
+    return (0);
+}
+
+ULONG APIENTRY DosSetRelMaxFH(LONG *req, ULONG *max)
+{
+    return (0);
 }
