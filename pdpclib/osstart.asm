b58a5e2ea37aa0ab24d10132ef6df8fa63e8fb16
Paul Edwards <mutazilah@gmail.com>
2024-03-01 16:58:46 +0800

    fix comment

--- pdpclib/osstart.asm
+++ pdpclib/osstart.asm
@@ -37,7 +37,7 @@ top:
 
 public __main
 __intstart proc
-# We no longer use this copy of the command line
+; We no longer use this copy of the command line
         mov eax, [esp+16]
         push eax
         call __start
