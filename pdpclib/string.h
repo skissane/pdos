e254504d37f37b413746d9fa6922de3a122572b1
Paul Edwards <mutazilah@gmail.com>
2024-03-10 16:50:16 +0800

    mods for OS/2 to eliminate warnings about builtins

--- pdpclib/string.h
+++ pdpclib/string.h
@@ -31,7 +31,7 @@ typedef unsigned long size_t;
 typedef unsigned long long size_t;
 #endif
 
-#elif (defined(__OS2__) || defined(__MVS__) \
+#elif (defined(__MVS__) \
     || defined(__CMS__) || defined(__VSE__) || defined(__SMALLERC__) \
     || defined(__ARM__) || defined(__gnu_linux__) \
     || defined(__SZ4__))
@@ -42,7 +42,7 @@ typedef unsigned long size_t;
 /* typedef unsigned int size_t; */
 #elif (defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC) \
     || defined(__WIN32__) || defined(__AMIGA__) || defined(__EFI__) \
-    || defined(__32BIT__) || defined(__PDOS386__))
+    || defined(__OS2__) || defined(__32BIT__) || defined(__PDOS386__))
 typedef unsigned int size_t;
 #endif
 #endif
