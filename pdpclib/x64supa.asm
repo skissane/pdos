46963327ac88b7c81ac37cb18c2adff27575eddc
Paul Edwards <mutazilah@gmail.com>
2024-03-20 23:19:16 +0800

    allow assembly with pdas too

--- pdpclib/x64supa.asm
+++ pdpclib/x64supa.asm
@@ -1,3 +1,4 @@
+ifndef PDAS
 ; Written by Paul Edwards
 ; Released to the public domain
 
@@ -8,8 +9,22 @@
 ; plus return address
 ; And first location is reserved for return value
 ; (second is for return address)
+
+else
+.code64
+.intel_syntax noprefix
+endif
+
+
+
+ifndef PDAS
 public __setj
 __setj proc
+else
+.globl __setj
+__setj:
+endif
+
 mov [rcx + 8*2], rbx
 mov [rcx + 8*3], rcx
 mov [rcx + 8*4], rdx
@@ -18,15 +33,31 @@ mov [rcx + 8*6], r9
 mov [rcx + 8*7], rsp
 mov rax, [rsp]
 mov [rcx + 8*1], rax
+
+ifndef PDAS
 ; we only return int (eax), but since we're not preserving
 ; rax anyway (maybe we should?), may as well clear it.
+endif
+
 xor rax,rax
 ret
-__setj endp
 
+ifndef PDAS
+__setj endp
+endif
+
+
+
+
+ifndef PDAS
 ; I think rcx will have env
 public __longj
 __longj proc
+else
+.globl __longj
+__longj:
+endif
+
 mov rax, [rcx + 8*7]
 mov rsp, rax
 mov rax, [rcx + 8*1]
@@ -38,16 +69,50 @@ mov r9, [rcx + 8*6]
 mov rax, [rcx]
 mov rcx, [rcx + 8*3]
 ret
-__longj endp
 
+ifndef PDAS
+__longj endp
+endif
+
+
+
+
+ifndef PDAS
 public __main
 __main proc
+else
+.globl __main
+__main:
+endif
+
 	ret
+
+ifndef PDAS
 __main endp
+endif
+
+
+
 
 .data
 
+ifndef PDAS
 public _fltused
 _fltused dd ?
+else
+_fltused:
+.space 4
+endif
 
+
+
+ifndef PDAS
+; this very odd syntax for "end" is required so that it is ignored
+; by pdas, but accepted by masm. masm requires the "end" and won't
+; accept the "end" within an ifdef block
+endif
+
+
+; /*
 end
+; */
