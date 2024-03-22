f1a63e2e7de0580715eb9dd57bd55c4099486cb4
Paul Edwards <mutazilah@gmail.com>
2024-03-22 15:53:04 +0800

    allow the stack position to be optionally provided

--- pdpclib/linstart.c
+++ pdpclib/linstart.c
@@ -12,11 +12,18 @@ extern int __start(char *p);
 
 extern int __exita(int rc);
 
-/* For Linux environments, we ignore the data on the (abnormal) stack
+/* For Linux environments, we (normally) ignore the data on the
+   (abnormal) stack
    and get parameters from /proc/<pid>/command instead. This way the
    executable can be run under PDOS/386 which does not create a
    silly stack. Note that the Linux stack has no return address,
-   and instead has a count, followed by all the parameters as pointers */
+   and instead has a count, followed by all the parameters as pointers.
+
+   However, because /proc is not available in early Linux userspace
+   or in a chroot() jail, we allow the possibility of an assembler
+   stub to be used, that will call this function with the address
+   of the stack on entry, and then further code can take its chances
+   with interpreting it if /proc/... can't be opened */
 
 #ifdef __MACOS__
 /* The dynamic loader is getting first crack so that we
@@ -33,11 +40,11 @@ int _start(int argc, char **argv)
 
 #else
 
-int _start(void)
+int _start(char *p)
 {
     int rc;
 
-    rc = __start(NULL);
+    rc = __start(p);
     __exita(rc);
 
     /* normal Unix environments don't have a valid return address,
