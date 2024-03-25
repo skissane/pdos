8980a707324af75c61988864b4139720104d34fc
Paul Edwards <mutazilah@gmail.com>
2024-03-25 21:44:14 +0800

    support code for Rust

---
+++ rust/extra.c
@@ -0,0 +1,28 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  extra - extra funtions for Rust                                  */
+/*                                                                   */
+/*********************************************************************/
+
+#include <stdio.h>
+
+#define lang_start_internal _ZN3std2rt19lang_start_internal17hfefa8592b280fcd4E
+#define stdio_print _ZN3std2io5stdio6_print17h06e585553ed71412E
+
+void lang_start_internal(void (**x)(void))
+{
+    (*x)();
+    return;
+}
+
+void stdio_print(int x, int y, int z, char *str)
+{
+    printf("%s", str);
+    return;
+}
