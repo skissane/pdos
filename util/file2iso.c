6bce4f5e085469a2eefc7ef568ee324efcce4548
Paul Edwards <mutazilah@gmail.com>
2024-04-10 16:12:52 +0800

    more corrections

--- util/file2iso.c
+++ util/file2iso.c
@@ -10,16 +10,22 @@
 /*                                                                   */
 /*********************************************************************/
 
-/* Documentation for the El Torito spec can be found here:
+/* Documentation for ISO 9660 can be found here:
 
-https://web.archive.org/web/20180112220141/
-https://download.intel.com/support/motherboards/desktop/sb/specscdrom.pdf
+https://wiki.osdev.org/ISO_9660
 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define ROOT_LBN 17
+#define DIR_LBN ROOT_LBN + 2
+#define FILE_LBN DIR_LBN + 1
+
+static void both_end32(unsigned char *p, unsigned long val);
+static void both_end16(unsigned char *p, unsigned long val);
 
 int main(int argc, char **argv)
 {
@@ -34,6 +40,7 @@ int main(int argc, char **argv)
     int fnm_len;
     int tot_len;
     long len_file;
+    long blocks;
 
     if (argc <= 2)
     {
@@ -61,12 +68,52 @@ int main(int argc, char **argv)
         fwrite(buf, sizeof buf, 1, fq);
     }
 
+
+
+    /* calculate what we will need */
+    fnm_len = strlen(fnm);
+    fnm_len += 2; /* for ;1 terminator */
+    if ((fnm_len % 2) == 0)
+    {
+        fnm_len++;
+    }
+    tot_len = 33 + fnm_len;
+    if ((tot_len % 2) != 0)
+    {
+        tot_len++;
+    }
+    fseek(fp, 0, SEEK_END);
+    len_file = ftell(fp);
+    rewind(fp);
+
+    blocks = len_file / 2048;
+    if ((len_file % 2048) != 0)
+    {
+        blocks++;
+    }
+    blocks += FILE_LBN;
+
+
     /* primary volume descriptor */
     memset(buf, '\0', sizeof buf);
     buf[0] = 1;
     memcpy(buf + 1, "CD001", 5);
     buf[6] = 1;
+    /* root directory entry */
+    p = buf + 156;
+    p[0] = 34;
+    p += 2;
+    both_end32(p, DIR_LBN);
+    p += 8;
+    both_end32(p, tot_len);
+    p += 8 + 7;
+    *p = 1 << 1; /* mark it as a directory - right? */
+    both_end32(buf + 80, blocks);
+    both_end16(buf + 120, 1);
+    both_end16(buf + 124, 1);
+    buf[881] = 1;
     fwrite(buf, sizeof buf, 1, fq);
+
 
     /* volume descriptor set terminator */
     memset(buf, '\0', sizeof buf);
@@ -77,37 +124,25 @@ int main(int argc, char **argv)
 
 
 
+    /* I'm hoping I don't need a path table */
+
+
 
 
     /* need directory info now */
     memset(buf, '\0', sizeof buf);
-    fnm_len = strlen(fnm);
-    if ((fnm_len % 2) == 0)
-    {
-        fnm_len++;
-    }
-    tot_len = 33 + fnm_len;
-    if ((tot_len % 2) != 0)
-    {
-        tot_len++;
-    }
     p = buf;
     p[0] = tot_len;
-    /* 8 bytes to give the logical block number
-       presumably in ASCII */
     p = buf + 2;
-    sprintf(p, "%8d", 19); /* 19 is somewhat of a guess */
+    both_end32(p, FILE_LBN);
 
-    /* length in bytes of the file. Is it printed decimal or hex or neither? */
-    fseek(fp, 0, SEEK_END);
-    len_file = ftell(fp);
-    rewind(fp);
     p = buf + 10;
-    sprintf(p, "%8ld", len_file);
+    both_end32(p, len_file);
 
     buf[32] = fnm_len;
     p = buf + 33;
     strcpy(p, fnm);
+    strcat(p, ";1");
     while (*p != '\0')
     {
         *p = toupper((unsigned char)*p);
@@ -138,4 +173,24 @@ int main(int argc, char **argv)
     }
     printf("success\n");
     return (0);
+}
+
+static void both_end32(unsigned char *p, unsigned long val)
+{
+    p[0] = p[7] = val & 0xff;
+    val >>= 8;
+    p[1] = p[6] = val & 0xff;
+    val >>= 8;
+    p[2] = p[5] = val & 0xff;
+    val >>= 8;
+    p[3] = p[4] = val & 0xff;
+    return;
+}
+
+static void both_end16(unsigned char *p, unsigned long val)
+{
+    p[0] = p[3] = val & 0xff;
+    val >>= 8;
+    p[1] = p[2] = val & 0xff;
+    return;
 }
