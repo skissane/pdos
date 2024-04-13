5be37ca0ff2945d84eff36d7fca852edfc613c74
Paul Edwards <mutazilah@gmail.com>
2024-04-13 23:08:53 +0800

    stop defining ARM64 __exit as noreturn

--- pdpclib/stdlib.c
+++ pdpclib/stdlib.c
@@ -754,7 +754,8 @@ __PDPCLIB_API__ void abort(void)
 
 #if !defined(__EMX__) && !defined(__GNUC__) && !defined(__gnu_linux__) \
     && !defined(__ARM__) \
-    || defined(WATLIN)
+    || defined(WATLIN) \
+    || (defined(__ARM__) && defined(__64BIT__))
 void __exit(int status);
 #else
 void __exit(int status) __attribute__((noreturn));
