725ad206ebe046997c749acd2b4eb0eb978d14b9
Paul Edwards <mutazilah@gmail.com>
2024-04-25 13:48:07 +0800

    save/restore more registers in setjmp/longjmp

--- pdpclib/a64supa.asm
+++ pdpclib/a64supa.asm
@@ -45,6 +45,29 @@ ___Ysetjmp:
         str     x10,[x1,#36*2]
 #  @ sl
 
+        str     x13,[x1,#14*4*2]
+        str     x14,[x1,#15*4*2]
+        str     x15,[x1,#16*4*2]
+        str     x16,[x1,#17*4*2]
+        str     x17,[x1,#18*4*2]
+        str     x18,[x1,#19*4*2]
+        str     x19,[x1,#20*4*2]
+        str     x20,[x1,#21*4*2]
+        str     x21,[x1,#22*4*2]
+        str     x22,[x1,#23*4*2]
+        str     x23,[x1,#24*4*2]
+        str     x24,[x1,#25*4*2]
+        str     x25,[x1,#26*4*2]
+        str     x26,[x1,#27*4*2]
+        str     x27,[x1,#28*4*2]
+        str     x28,[x1,#29*4*2]
+        str     x29,[x1,#30*4*2]
+        str     x30,[x1,#31*4*2]
+
+# This doesn't assemble
+# The distance 32 appears to be too far
+#        str     x31,[x1,#32*4*2]
+
         mov     x0, #0
         ret
 
@@ -89,6 +112,27 @@ notzero:
         ldr     x3,[x1,#44*2]
         ldr     x12,[x1,#48*2]
 #  @ ip
+
+        ldr     x13,[x1,#14*4*2]
+        ldr     x14,[x1,#15*4*2]
+        ldr     x15,[x1,#16*4*2]
+        ldr     x16,[x1,#17*4*2]
+        ldr     x17,[x1,#18*4*2]
+        ldr     x18,[x1,#19*4*2]
+        ldr     x19,[x1,#20*4*2]
+        ldr     x20,[x1,#21*4*2]
+        ldr     x21,[x1,#22*4*2]
+        ldr     x22,[x1,#23*4*2]
+        ldr     x23,[x1,#24*4*2]
+        ldr     x24,[x1,#25*4*2]
+        ldr     x25,[x1,#26*4*2]
+        ldr     x26,[x1,#27*4*2]
+        ldr     x27,[x1,#28*4*2]
+        ldr     x28,[x1,#29*4*2]
+        ldr     x29,[x1,#30*4*2]
+        ldr     x30,[x1,#31*4*2]
+#        ldr     x31,[x1,#32*4*2]
+
         ldr     x1,[x1,#52*2]
 #        mov     pc,lr
         ret
