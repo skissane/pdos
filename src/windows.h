a5c4a2fc373bf5f73525cd33e75c1831ca7e1298
Paul Edwards <mutazilah@gmail.com>
2024-05-06 19:50:46 +0800

    better definition of VirtualAlloc/Free

--- src/windows.h
+++ src/windows.h
@@ -1449,9 +1449,7 @@ BOOL WINAPI UpdateWindow (HWND hWnd);
 #define GetModuleHandle GetModuleHandleA
 HMODULE WINAPI GetModuleHandleA (LPCSTR lpModuleName);
 
-
-
-/* unknown header */
+/* Subheader: memoryapi.h */
 
 #define MEM_RELEASE 0x8000
 #define MEM_COMMIT 0x1000
@@ -1459,5 +1457,11 @@ HMODULE WINAPI GetModuleHandleA (LPCSTR lpModuleName);
 #define PAGE_EXECUTE_READWRITE 0x40
 #define PAGE_READWRITE 0x4
 
-BOOL VirtualFree(LPVOID a, SIZE_T b, DWORD c);
-LPVOID VirtualAlloc(LPVOID a, SIZE_T b, DWORD c, DWORD d);
+LPVOID WINAPI VirtualAlloc (LPVOID lpAddress,
+                            SIZE_T dwSize,
+                            DWORD flAllocationType,
+                            DWORD flProtect);
+
+BOOL WINAPI VirtualFree (LPVOID lpAddress,
+                         SIZE_T dwSize,
+                         DWORD dwFreeType);
