721300c6b4ebbbfbb87844c6bdf04854bf789c02
Paul Edwards <mutazilah@gmail.com>
2024-05-16 05:25:45 +0800

    add dummy functions to allow linking

--- pdpclib/l68supa.asm
+++ pdpclib/l68supa.asm
@@ -56,4 +56,56 @@ zzz:    jmp zzz
 msg: dc.b 'H', 'i', '\n'
 
 
+.text
+
+.globl ___main
+___main:
+.globl ___open
+___open:
+.globl ___close
+___close:
+.globl ___read
+___read:
+.globl ___write
+___write:
+.globl ___rename
+___rename:
+.globl ___seek
+___seek:
+.globl ___remove
+___remove:
+.globl ___mmap
+___mmap:
+.globl ___munmap
+___munmap:
+.globl ___clone
+___clone:
+.globl ___waitid
+___waitid:
+.globl ___execve
+___execve:
+.globl ___exita
+___exita:
+.globl ___ioctl
+___ioctl:
+.globl ___getpid
+___getpid:
+.globl ___open
+___open:
+.globl ___read
+___read:
+.globl ___close
+___close:
+.globl ___exita
+___exita:
+.globl ___exita
+___exita:
+.globl ___ioctl
+___ioctl:
+.globl ___time
+___time:
+
+rts
+
+
 .endif
