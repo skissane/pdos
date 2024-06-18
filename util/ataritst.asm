760d70844f0f55b3c4f4a64b37cd48b60970072f
Paul Edwards <mutazilah@gmail.com>
2024-06-18 22:32:07 +0800

    test of Atari TOS

---
+++ util/ataritst.asm
@@ -0,0 +1,40 @@
+# Simple Motorola 68000 program for Atari TOS
+
+# Written by Paul Edwards
+# Released the public domain
+
+#asami -o ataritst.o ataritst.asm
+#pdld -s --oformat atari -o ataritst.exe ataritst.o
+#atariemu ataritst.exe
+
+
+# a7=sp (stack)
+
+
+        .text
+
+        .globl  myentry
+myentry:
+
+#        sub    sp, #4
+        lea -4(sp), sp
+
+# 4 = TOS Cconout syscall
+        move.w #4, 0(sp)
+
+        move.w 'H', 2(sp)
+        trap #1
+
+        move.w 'i', 2(sp)
+        trap #1
+
+        move.w '\r', 2(sp)
+        trap #1
+
+        move.w '\n', 2(sp)
+        trap #1
+
+#        add    sp, #4
+        lea 4(sp), sp
+
+zzz:    jmp zzz
