030b274ba93481dda7e0ae667d74d570e5b4c14b
Paul Edwards <mutazilah@gmail.com>
2024-07-10 19:22:29 +0800

    call runmvs

--- generic/mfbios.bat
+++ generic/mfbios.bat
@@ -4,7 +4,7 @@ rem You need to have run pdpzpb.bat in pdpclib first
 gccmvs -Os -U__MVS__ -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios bios.c
 gccmvs -Os -U__MVS__ -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios ../bios/exeload.c
 m4 -I . -I ../pdpclib mfbios.m4 >mfbios.jcl
-rem call runmvs mfbios.jcl output.txt none bios.exe
+call runmvs mfbios.jcl output.txt none bios.exe
 rem we need to signal to sapstart that this is already loaded
 rem in memory so it doesn't need to attempt to self-load
 rem this offset will change if sapstart ever changes
