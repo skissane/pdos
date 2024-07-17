f2525212801a4a2d233f0c0bdaf767f731537a05
Paul Edwards <mutazilah@gmail.com>
2024-07-17 20:34:02 +0800

    extra directory for include files now required

--- generic/mfbios.bat
+++ generic/mfbios.bat
@@ -1,8 +1,8 @@
 rem build z/pdos-generic pseudo-bios
 rem You need to have run pdpzpb.bat in pdpclib first
 
-gccmvs -Os -U__MVS__ -DNEED_MVS -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios bios.c
-gccmvs -Os -U__MVS__ -DNEED_MVS -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios ../bios/exeload.c
+gccmvs -Os -U__MVS__ -DNEED_MVS -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios -I ../src bios.c
+gccmvs -Os -U__MVS__ -DNEED_MVS -D__MF32__ -D__ZPDOSGPB__ -DUSE_MEMMGR -S -I . -I ../pdpclib -I ../bios -I ../src ../bios/exeload.c
 m4 -I . -I ../pdpclib mfbios.m4 >mfbios.jcl
 call runmvs mfbios.jcl output.txt none bios.exe
 rem we need to signal to sapstart that this is already loaded
