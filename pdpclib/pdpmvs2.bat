de021b0531fb1604d213c0bff9f4265299691fe4
Paul Edwards <mutazilah@gmail.com>
2024-09-02 18:07:05 +0800

    compile more things

--- pdpclib/pdpmvs2.bat
+++ pdpclib/pdpmvs2.bat
@@ -1,5 +1,14 @@
 rem Produces an MVS PE executable and then executes it
 
-as370 -a=list.txt -o mfsupa.obj mfsupa.asm
-pdld --oformat mainframe -o pdptest.exe mfsupa.obj
+as370 -o mfsupa.obj mfsupa.asm
+
+gcc370 -S -o mfsupc.s mfsupc.c
+as370 -o mfsupc.obj mfsupc.s
+rm -f mfsupc.s
+
+gcc370 -S -I . -o string.s string.c
+as370 -o string.obj string.s
+rm -f string.s
+
+pdld -e __crt0 --oformat mainframe -o pdptest.exe mfsupa.obj mfsupc.obj
 call runmvs pdpmvs2.jcl output.txt pdptest.exe
