f62d050dea287a5bf627f6b4f600b78093a5aee4
Paul Edwards <mutazilah@gmail.com>
2024-09-02 13:29:26 +0800

    mainframe support code in assembler

---
+++ pdpclib/mfsupa.asm
@@ -0,0 +1,12 @@
+; mfsupa - mainframe support code in assembler
+; Written by Paul Edwards
+; Released to the public domain
+
+         .text
+         .balign 4
+         .using .,r15
+;         MVC   92(8,r13),=D'1.0E0'
+         L     r15,=F'3'
+         BR    r14
+         .balign 8
+         .ltorg
