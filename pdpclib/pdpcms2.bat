4c5c4e12e7216afbcda9cb77ceca79a32dd80832
Paul Edwards <mutazilah@gmail.com>
2024-09-09 09:21:54 +0800

    switch to building with makefile.370

--- pdpclib/pdpcms2.bat
+++ pdpclib/pdpcms2.bat
@@ -1,14 +1,5 @@
 rem Produces a CMS executable (including RDWs) and then executes it
 
-as370 -o mfsupa.obj mfsupa.asm
+pdmake -B -f makefile.370
 
-gcc370 -S -o mfsupc.s mfsupc.c
-as370 -o mfsupc.obj mfsupc.s
-rm -f mfsupc.s
-
-gcc370 -S -I . -o string.s string.c
-as370 -o string.obj string.s
-rm -f string.s
-
-pdld -e __crt0 --oformat cms -o pdptest.exe mfsupa.obj mfsupc.obj
 call runcms pdpcms2.exec output.txt pdptest.exe
