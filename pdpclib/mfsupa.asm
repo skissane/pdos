d027f9adfb29fdbefb9c4fa948d897c4e6cf3a26
Paul Edwards <mutazilah@gmail.com>
2024-09-10 18:55:25 +0800

    start calling C code as part of DOS/VS testing

--- pdpclib/mfsupa.asm
+++ pdpclib/mfsupa.asm
@@ -4,15 +4,34 @@
 
          .text
          .balign 2
-         .using .,r15
+         .extern __ret6
+         .using .,r12
 #        MVC   92(8,r13),=D'1.0E0'
          .globl __crt0
 __crt0:
+         LR    r12,r15
+         LR    r7,r14
+         L     r3,=F'3'
+         L     r4,=F'4'
+         L     r5,=F'5'
+         LA    r13,savea
+         ST    r13,76(,r13)
+         L     r15,=V(__ret6)
+#         .long 0
+#         .long 0xcccccccc
+#         .long 0xcccccccc
+         BALR  r14,r15
+.LABC:
+         B     .LABC
          L     r15,=F'3'
+         LR    r14,r7
          BR    r14
          .balign 8
          .ltorg
-         .drop r15
+         .drop r12
+         .balign 4
+savea:
+         .space 4000
 
 
 
