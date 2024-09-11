f0ab97f7fe04c2883e10fa0b628e9ab99a311490
Paul Edwards <mutazilah@gmail.com>
2024-09-11 08:12:05 +0800

    create listings and map

--- pdpclib/pdpvse2.bat
+++ pdpclib/pdpvse2.bat
@@ -1,14 +1,15 @@
 rem Produces a VSE executable and then executes it
 
-as370 -o mfsupa.obj mfsupa.asm
+as370 -a=list.txt -o mfsupa.obj mfsupa.asm
 
-gcc370 -S -o mfsupc.s mfsupc.c
-as370 -o mfsupc.obj mfsupc.s
+gcc370 -I . -S -o mfsupc.s mfsupc.c
+as370 -a=list2.txt -o mfsupc.obj mfsupc.s
 rm -f mfsupc.s
 
 gcc370 -S -I . -o string.s string.c
 as370 -o string.obj string.s
 rm -f string.s
 
-pdld -e __crt0 --oformat vse -o pdptest.exe mfsupa.obj mfsupc.obj
+pdld -Map map.txt --image-base 0x7000c8 -e __crt0 --oformat vse -o pdptest.exe mfsupa.obj mfsupc.obj
+
 call runvse pdpvse2.jcl output.txt pdptest.exe
