9a69e25c341cb621da896105dbbdf871fd1cbcf8
Paul Edwards <mutazilah@gmail.com>
2024-09-12 04:56:07 +0800

    do some cleanup

--- pdpclib/mfsupa.asm
+++ pdpclib/mfsupa.asm
@@ -14,32 +14,33 @@
 #        MVC   92(8,r13),=D'1.0E0'
          .globl __crt0
 __crt0:
-         B     .skiphdr
+         B     skiphdr
 #         .byte "PGCX"  # PDOS-generic (or compatible) extension
 # Needs to be in EBCDIC
          .byte 0xd7, 0xc7, 0xc3, 0xe7
-         .long 4   # length of header data
+#         .long 4   # length of header data
+         .DC   F'4'
          .globl __pgparm
 __pgparm: .long 0   # This will be zapped by z/PDOS-generic if running under it
-.skiphdr:
-         L     r1,=V(__pgparm)
-         L     r1,0(,r1)
-         LTR   r1,r1
-         BZ    .Lnotpdos
+skiphdr:
+         L     R1,=V(__pgparm)
+         L     R1,0(,R1)
+         LTR   R1,R1
+         BZ    notpdos
          STM   r14,r12,12(r13)
          LA    r13,80(,r13)
          LR    r12,r15
          .drop r15
          .using __crt0, r12
-         B     bypass1
-.Lnotpdos:
+         B     BYPASS1
+notpdos:
          LR    r12,r15
          LR    r7,r14
          L     r3,=F'3'
          L     r4,=F'4'
          L     r5,=F'5'
          LA    r13,savea
-bypass1:
+BYPASS1:
          LA    r9,80(,r13)
          ST    r9,76(,r13)
          L     r15,=V(__vserun)
@@ -55,11 +56,11 @@ bypass1:
          L     r1,=V(__pgparm)
          L     r1,0(,r1)
          LTR   r1,r1
-         BZ    .Lnotpdos2
+         BZ    notpdos2
          S     r13,=F'80'
 # Need r15 logic
          LM    r14,r12,12(r13)
-.Lnotpdos2:
+notpdos2:
          BR    r14
          .balign 8
          .ltorg
@@ -83,11 +84,11 @@ __svcreal:
          L     r0,0(,r8)
          L     r1,4(,r8)
          L     r15,60(,r8)
-         EX    r7, .LSVC1
-         B     .LSVC2
-.LSVC1:
+         EX    r7, SVC1
+         B     SVC2
+SVC1:
          SVC   0
-.LSVC2:
+SVC2:
          LM    r14,r12,12(r13)
          BR    r14
 
