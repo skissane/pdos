13a3dde5c500ffe3adcc2bde19607fe734e4dc3a
Paul Edwards <mutazilah@gmail.com>
2024-09-11 08:11:49 +0800

    support WTO for VSE

--- pdpclib/mfsupc.c
+++ pdpclib/mfsupc.c
@@ -6,6 +6,7 @@
    used as part of a C library. Do a #define if not
    part of a C library */
 
+#include <string.h>
 
 #include "__mvs.h"
 
@@ -31,4 +32,65 @@ int __wto(int len, int flags, char *msg)
 int __ret6(void)
 {
     return (6);
+}
+
+
+/* C startup code for VSE */
+
+int __vserun(void)
+{
+    /* issue a WTO for now */
+    /* not sure what alignment a CCW needs */
+    union {
+        double dummy;
+        struct {
+            unsigned int addr;
+            unsigned short flags;
+            unsigned short len;
+        } actual;
+    } ccw;
+    char msg[80];
+    /* not sure what alignment a CCB needs */
+    union {
+        double dummy;
+        struct {
+            unsigned short residual_count;
+            unsigned short communication_bytes;
+            unsigned short csw_status_bytes;
+            unsigned char logical_unit_class;
+            unsigned char logical_unit;
+            void *ccw_address;
+            unsigned int status_and_csw_ccw;
+        } actual;
+    } ccb;
+    regs regsin;
+    regs regsout;
+
+    memcpy(msg, "MADE IT!", 8);
+    ccw.actual.addr = (0x09 << 24) | (unsigned int)msg;
+    ccw.actual.flags = 0;
+    ccw.actual.len = 8;
+    
+    ccb.actual.residual_count = 0;
+    ccb.actual.communication_bytes = 0;
+    ccb.actual.csw_status_bytes = 0;
+    ccb.actual.logical_unit_class = 0;
+    ccb.actual.logical_unit = 4; /* I guess this means SYSLOG */
+    ccb.actual.ccw_address = &ccw;
+    ccb.actual.status_and_csw_ccw = 0;
+
+    /* excp ccb */
+    regsin.r[1] = (int)&ccb;
+    __svc(0, &regsin, &regsout);
+
+    /* wait ccb */
+    /* so long as the top bit is 0, the EXCP was accepted and
+       thus it is OK to wait on it - I think */
+    if ((ccb.actual.csw_status_bytes & 0x8000U) == 0)
+    {
+        regsin.r[1] = (int)&ccb;
+        __svc(7, &regsin, &regsout);
+    }
+
+    return (0);
 }
