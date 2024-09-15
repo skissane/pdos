4e40bad23ce2f8bff53f012d2ddfd528614e10ce
Paul Edwards <mutazilah@gmail.com>
2024-09-15 09:57:26 +0800

    specify MVS build

--- pdpclib/pdpmvs2.bat
+++ pdpclib/pdpmvs2.bat
@@ -1,8 +1,8 @@
 rem Produces an MVS PE executable and then executes it
 
-as370 -o mfsupa.obj mfsupa.asm
+as370 --defsym MVS=1 -o mfsupa.obj mfsupa.asm
 
-gcc370 -S -o mfsupc.s mfsupc.c
+gcc370 -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
 as370 -o mfsupc.obj mfsupc.s
 rm -f mfsupc.s
 
@@ -10,5 +10,6 @@ gcc370 -S -I . -o string.s string.c
 as370 -o string.obj string.s
 rm -f string.s
 
-pdld -e __crt0 --oformat mainframe -o pdptest.exe mfsupa.obj mfsupc.obj
+pdld -e __crt0 --oformat mvs -o pdptest.exe mfsupa.obj mfsupc.obj
+
 call runmvs pdpmvs2.jcl output.txt pdptest.exe
