35b751eb5d5e8e8d8c260d4b6922d94177fd2241
Paul Edwards <mutazilah@gmail.com>
2024-09-16 22:28:45 +0800

    switch on optimization etc

--- pdpclib/pdpmvs2.bat
+++ pdpclib/pdpmvs2.bat
@@ -2,14 +2,16 @@ rem Produces an MVS PE executable and then executes it
 
 as370 --defsym MVS=1 -o mfsupa.obj mfsupa.asm
 
-gcc370 -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
+gcc370 -Os -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
 as370 -o mfsupc.obj mfsupc.s
 rm -f mfsupc.s
 
-gcc370 -S -I . -o string.s string.c
+gcc370 -Os -S -I . -o string.s string.c
 as370 -o string.obj string.s
 rm -f string.s
 
-pdld -e __crt0 --oformat mvs -o pdptest.exe mfsupa.obj mfsupc.obj
+pdld -e __crt0 --oformat mvs -o pdptest.exe mfsupa.obj mfsupc.obj string.obj
+
+sleep 2
 
 call runmvs pdpmvs2.jcl output.txt pdptest.exe
