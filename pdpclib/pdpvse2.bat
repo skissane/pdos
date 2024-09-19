b54013d49e26f70a0bb11df2c5ba1ec3720f790a
Paul Edwards <mutazilah@gmail.com>
2024-09-19 12:33:22 +0800

    start using C preprocessor

--- pdpclib/pdpvse2.bat
+++ pdpclib/pdpvse2.bat
@@ -1,6 +1,7 @@
 rem Produces a VSE executable and then executes it
 
-as370 -a=list.txt --defsym VSE=1 -o mfsupa.obj mfsupa.asm
+pdcc -N -D VSE -o mfsupa.s mfsupa.asm
+as370 -mhlasm -a=list.txt -o mfsupa.obj mfsupa.s
 
 gcc370 -U__MVS__ -D__VSE__ -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
 as370 -a=list2.txt -o mfsupc.obj mfsupc.s
