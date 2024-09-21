2c56c75a78347b9ed43cfaa8aa0147957b5f3026
Paul Edwards <mutazilah@gmail.com>
2024-09-21 18:14:26 +0800

    switch to HLASM syntax

--- pdpclib/zpgsupa.asm
+++ pdpclib/zpgsupa.asm
@@ -1,45 +1,48 @@
-# This program written by Paul Edwards.
-# Released to the public domain
-
-#***********************************************************************
-#*                                                                     *
-#*  SETJ - SAVE REGISTERS INTO ENV                                     *
-#*                                                                     *
-#***********************************************************************
-         .globl __setj
-__setj:
+* This program written by Paul Edwards.
+* Released to the public domain
+*
+         CSECT
+*
+***********************************************************************
+*                                                                     *
+*  SETJ - SAVE REGISTERS INTO ENV                                     *
+*                                                                     *
+***********************************************************************
+         ENTRY @@SETJ
+@@SETJ   DS    0H
          L     R15,0(,R1)         # get the env variable
          STM   R0,R14,0(R15)      # save registers to be restored
          LA    R15,0(,R0)         # setjmp needs to return 0
          BR    R14                # return to caller
-#***********************************************************************
-#*                                                                     *
-#*  LONGJ - RESTORE REGISTERS FROM ENV                                 *
-#*                                                                     *
-#***********************************************************************
-         .globl __longj
-__longj:
+*
+***********************************************************************
+*                                                                     *
+*  LONGJ - RESTORE REGISTERS FROM ENV                                 *
+*                                                                     *
+***********************************************************************
+         ENTRY @@LONGJ
+@@LONGJ  DS    0H
          L     R2,0(,R1)          # get the env variable
          L     R15,60(,R2)        # get the return code
          LM    R0,R14,0(R2)       # restore registers
          BR    R14                # return to caller
-#
-#***********************************************************************
-#*                                                                     *
-#*  GETCLCK - GET THE VALUE OF THE MVS CLOCK TIMER AND MOVE IT TO AN   *
-#*  8-BYTE FIELD.  THIS 8-BYTE FIELD DOES NOT NEED TO BE ALIGNED IN    *
-#*  ANY PARTICULAR WAY.                                                *
-#*                                                                     *
-#*  E.G. CALL 'GETCLCK' USING WS-CLOCK1                                *
-#*                                                                     *
-#*  THIS FUNCTION ALSO RETURNS THE NUMBER OF SECONDS SINCE 1970-01-01  *
-#*  BY USING SOME EMPIRICALLY-DERIVED MAGIC NUMBERS                    *
-#*                                                                     *
-#***********************************************************************
-#*
-         .globl __getclk
-         .using __getclk, r15
-__getclk:
+*
+***********************************************************************
+*                                                                     *
+*  GETCLCK - GET THE VALUE OF THE MVS CLOCK TIMER AND MOVE IT TO AN   *
+*  8-BYTE FIELD.  THIS 8-BYTE FIELD DOES NOT NEED TO BE ALIGNED IN    *
+*  ANY PARTICULAR WAY.                                                *
+*                                                                     *
+*  E.G. CALL 'GETCLCK' USING WS-CLOCK1                                *
+*                                                                     *
+*  THIS FUNCTION ALSO RETURNS THE NUMBER OF SECONDS SINCE 1970-01-01  *
+*  BY USING SOME EMPIRICALLY-DERIVED MAGIC NUMBERS                    *
+*                                                                     *
+***********************************************************************
+*
+         ENTRY @@GETCLK
+         USING @@GETCLK,R15
+@@GETCLK DS    0H
          STM   R2,R5,12(R13)      # save a little
          LA    R3,32(,R13)        # use user's save area
          N     R3,=X'FFFFFFF8'    # on a double word boundary
@@ -56,5 +59,7 @@ __getclk:
          LM    R2,R5,12(R13)      # restore modified registers
          BR    R14                # return to caller
 *
-         .ltorg
-         .drop r15
+         LTORG
+         DROP  R15
+*
+         END
