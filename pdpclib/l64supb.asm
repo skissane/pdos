ffbad584955021091676a22512c3a1fd0a069477
Paul Edwards <mutazilah@gmail.com>
2024-10-16 13:44:38 +0800

    more registers to save in setjmp/longjmp

--- pdpclib/l64supb.asm
+++ pdpclib/l64supb.asm
@@ -635,6 +635,15 @@ mov [rcx + 8*4], rdx
 mov [rcx + 8*5], r8
 mov [rcx + 8*6], r9
 mov [rcx + 8*7], rsp
+mov [rcx + 8*8], rsi
+mov [rcx + 8*9], rdi
+mov [rcx + 8*10], r10
+mov [rcx + 8*11], r11
+mov [rcx + 8*12], r12
+mov [rcx + 8*13], r13
+mov [rcx + 8*14], r14
+mov [rcx + 8*15], r15
+mov [rcx + 8*16], rbp
 mov rax, [rsp]
 mov [rcx + 8*1], rax
 # we only return int (eax), but since we're not preserving
@@ -656,6 +665,15 @@ mov rbx, [rcx + 8*2]
 mov rdx, [rcx + 8*4]
 mov r8, [rcx + 8*5]
 mov r9, [rcx + 8*6]
+mov rsi, [rcx + 8*8]
+mov rdi, [rcx + 8*9]
+mov r10, [rcx + 8*10]
+mov r11, [rcx + 8*11]
+mov r12, [rcx + 8*12]
+mov r13, [rcx + 8*13]
+mov r14, [rcx + 8*14]
+mov r15, [rcx + 8*15]
+mov rbp, [rcx + 8*16]
 mov rax, [rcx]
 mov rcx, [rcx + 8*3]
 ret
