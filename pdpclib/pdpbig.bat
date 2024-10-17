589fdd5e4fcb2643bf05b88c46f460c11bb526a5
Paul Edwards <mutazilah@gmail.com>
2024-10-17 17:11:56 +0800

    add doco

--- pdpclib/pdpbig.bat
+++ pdpclib/pdpbig.bat
@@ -1,4 +1,8 @@
 rem Produces an ELF32 executable for Linux Bigfoot and then executes it
+rem For the execution to complete, you will need to do the following things:
+rem telnet localhost 3270
+rem ipl bigfoot.ins
+rem quit
 
 pdmake -f makefile.big
 
