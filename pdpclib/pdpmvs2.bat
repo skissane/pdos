9fbd6520fc53bf50b31ca746cc7b02909f0b62b6
Paul Edwards <mutazilah@gmail.com>
2024-11-16 13:47:00 +0800

    switch to gccmvs

--- pdpclib/pdpmvs2.bat
+++ pdpclib/pdpmvs2.bat
@@ -3,12 +3,12 @@ rem Produces an MVS PE executable and then executes it
 pdcc -N -D MVS -o mfsupa.s mfsupa.asm
 as370 -mhlasm -mebcdic -a=list.txt -o mfsupa.obj mfsupa.s
 
-gcc370 -Os -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
-as370 -o mfsupc.obj mfsupc.s
+gccmvs -Os -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
+as370 -mhlasm -mebcdic -o mfsupc.obj mfsupc.s
 rm -f mfsupc.s
 
-gcc370 -Os -S -I . -o string.s string.c
-as370 -o string.obj string.s
+gccmvs -Os -S -I . -o string.s string.c
+as370 -mhlasm -mebcdic -o string.obj string.s
 rm -f string.s
 
 pdld -e __crt0 --oformat mvs -o pdptest.exe mfsupa.obj mfsupc.obj string.obj
--- pdpclib/pdpvse2.bat
+++ pdpclib/pdpvse2.bat
@@ -3,12 +3,12 @@ rem Produces a VSE executable and then executes it
 pdcc -N -D VSE -o mfsupa.s mfsupa.asm
 as370 -mhlasm -mebcdic -a=list.txt -o mfsupa.obj mfsupa.s
 
-gcc370 -U__MVS__ -D__VSE__ -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
-as370 -a=list2.txt -o mfsupc.obj mfsupc.s
+gccmvs -U__MVS__ -D__VSE__ -I. -I../generic -I../src -S -o mfsupc.s mfsupc.c
+as370 -mhlasm -mebcdic -a=list2.txt -o mfsupc.obj mfsupc.s
 rm -f mfsupc.s
 
-gcc370 -S -I . -o string.s string.c
-as370 -o string.obj string.s
+gccmvs -S -I . -o string.s string.c
+as370 -mhlasm -mebcdic -o string.obj string.s
 rm -f string.s
 
 pdld -Map map.txt --image-base 0x7000c8 -e __crt0 --oformat vse -o pdptest.exe mfsupa.obj mfsupc.obj
