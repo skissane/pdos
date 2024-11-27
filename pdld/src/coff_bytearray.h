7865d0dd2f9da4d4080e1b6aa0df6f2c7bdb21bd
Paul Edwards <mutazilah@gmail.com>
2024-11-27 10:13:12 +0800

    provide short names

--- pdld/src/coff_bytearray.h
+++ pdld/src/coff_bytearray.h
@@ -8,6 +8,35 @@
  * commercial and non-commercial, without any restrictions, without
  * complying with any conditions and by any means.
  *****************************************************************************/ 
+
+#define read_struct_IMAGE_DOS_HEADER rsidh
+#define read_struct_coff_header rsch
+#define read_struct_optional_header rsoh
+#define read_struct_optional_header_plus rsohp
+#define read_struct_IMAGE_DATA_DIRECTORY rsidd
+#define read_struct_section_table_entry rssete
+#define read_struct_relocation_entry rsre
+#define read_struct_symbol_table_entry rssyte
+#define read_struct_aux_section_symbol rsass
+#define read_struct_string_table_header rssth
+#define read_struct_IMAGE_BASE_RELOCATION rsibr
+#define read_struct_IMAGE_ARCHIVE_MEMBER_HEADER rsiamh
+#define read_struct_IMPORT_OBJECT_HEADER rsioh
+
+#define write_struct_IMAGE_DOS_HEADER wsidh
+#define write_struct_coff_header wsch
+#define write_struct_optional_header wsoh
+#define write_struct_optional_header_plus wsohp
+#define write_struct_IMAGE_DATA_DIRECTORY wsidd
+#define write_struct_section_table_entry wsste
+#define write_struct_IMAGE_EXPORT_DIRECTORY wsied
+#define write_struct_EXPORT_Name_Pointer_Table wsenpt
+#define write_struct_EXPORT_Ordinal_Table wseot
+#define write_struct_IMPORT_Directory_Table wsidt
+#define write_struct_IMAGE_BASE_RELOCATION wsibr
+#define write_struct_IMAGE_ARCHIVE_MEMBER_HEADER wsiamh
+#define write_struct_IMPORT_OBJECT_HEADER wsioh
+
 void read_struct_IMAGE_DOS_HEADER (struct IMAGE_DOS_HEADER_internal *IMAGE_DOS_HEADER_internal, const void *memory);
 void read_struct_coff_header (struct coff_header_internal *coff_header_internal, const void *memory);
 void read_struct_optional_header (struct optional_header_internal *optional_header_internal, const void *memory);
