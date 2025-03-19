0eadb50011aabcbcbff510e588811bdc41cd7cbc
Paul Edwards <mutazilah@gmail.com>
2025-03-20 01:04:27 +1100

    link with maths library too

--- cc64/bstrap.bat
+++ cc64/bstrap.bat
@@ -1,4 +1,4 @@
-gcc -O2 -DBLDGENERIC -o cc64 cc64.c junk.c
+gcc -O2 -DBLDGENERIC -o cc64 cc64.c junk.c -lm
 
 rem don't attempt to get cc64 to rebuild itself
 rem because that requires the non-generic version
