6e751128924e677faa69d42634093c3faa4a2201
Paul Edwards <mutazilah@gmail.com>
2025-05-14 03:09:24 +1000

    switch to supportf.asm

--- src/compxc.bat
+++ src/compxc.bat
@@ -6,6 +6,6 @@ gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__32BIT__ pos.c
 pdas -o pos.obj pos.s
 del pos.s
 
-pdas -o support.obj support.s
+as86 -o supportf.obj supportf.asm
 
-pdld -o xcopy.exe ../pdpclib/w32start.obj xcopy.obj pos.obj support.obj ../pdpclib/msvcrt.lib
+pdld -o xcopy.exe ../pdpclib/w32start.obj xcopy.obj pos.obj supportf.obj ../pdpclib/msvcrt.lib
