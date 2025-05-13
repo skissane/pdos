609195af04461b517d3df79d3740eb62c4340261
Paul Edwards <mutazilah@gmail.com>
2025-05-14 02:57:37 +1000

    update with new tools

--- src/compxc.bat
+++ src/compxc.bat
@@ -1,11 +1,11 @@
 gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__WIN32__ xcopy.c
-aswin -o xcopy.o xcopy.s
+pdas -o xcopy.obj xcopy.s
 del xcopy.s
 
 gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__32BIT__ pos.c
-aswin -o pos.o pos.s
+pdas -o pos.obj pos.s
 del pos.s
 
-aswin -o support.o support.s
+pdas -o support.obj support.s
 
-ldwin -o xcopy.exe ../pdpclib/w32start.o xcopy.o pos.o support.o ../pdpclib/msvcrt.a
+pdld -o xcopy.exe ../pdpclib/w32start.obj xcopy.obj pos.obj support.obj ../pdpclib/msvcrt.lib
